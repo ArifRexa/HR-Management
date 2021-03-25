7588260b8c34ed492762864c4953598b7f2043c4
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2021-03-25 21:00:41 +0600

    remove all

--- src/job_board/serializers.py
+++ src/job_board/serializers.py
@@ -1,20 +1,20 @@
-from rest_framework import serializers
-
-from src.job_board.models import Job
-
-
-class JobsSerializer(serializers.Serializer):
-    class Meta:
-        model = Job
-        fields = ['id', 'title']
 # from rest_framework import serializers
 #
-# from .models import Job
+# from src.job_board.models import Job
 #
 #
-# class AddJob(serializers.ModelSerializer):
-#     image_url = serializers.SerializerMethodField('get_image_url')
-#
+# class JobsSerializer(serializers.Serializer):
 #     class Meta:
 #         model = Job
-#         fields = ('id', 'title')
+#         fields = ['id', 'title']
+# # from rest_framework import serializers
+# #
+# # from .models import Job
+# #
+# #
+# # class AddJob(serializers.ModelSerializer):
+# #     image_url = serializers.SerializerMethodField('get_image_url')
+# #
+# #     class Meta:
+# #         model = Job
+# #         fields = ('id', 'title')
