67b374504bef96f1f11e6e257d3d694c22681084
kmrifat <kmrifat@gmail.com>
2021-08-02 15:12:27 +0600

    added graph, disbale select2

--- src/employee/admin_mixin/EmployeeExtraUrls.py
+++ src/employee/admin_mixin/EmployeeExtraUrls.py
@@ -1,32 +1,43 @@
+import random
 import re
 
 from django.contrib import admin
 from django.contrib.admin.widgets import AdminDateWidget
+from django.core.exceptions import PermissionDenied
 from django.db.models import QuerySet, Sum
 from django import forms
+from django.db.models.functions import Coalesce
 from django.forms import SelectDateWidget
 from django.template.response import TemplateResponse
 from django.urls import path
 
 from employee.models import Employee
-from project_management.models import EmployeeProjectHour, ProjectHour
+from project_management.models import EmployeeProjectHour, ProjectHour, Project
 
 
 class FilterForm(forms.Form):
-    project_hour__date__gte = forms.DateField(label='', widget=AdminDateWidget())
-    project_hour__date__lte = forms.DateField(label=' ', widget=AdminDateWidget())
+    project_hour__date__gte = forms.DateField(label='', widget=AdminDateWidget(attrs={'readonly': 'readonly'}))
+    project_hour__date__lte = forms.DateField(label=' ', widget=AdminDateWidget(attrs={'readonly': 'readonly'}))
 
 
 class EmployeeExtraUrls(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         employee_urls = [
+            path('graph/', self.admin_site.admin_view(self.all_employee_hour_graph_view)),
             path('<int:employee_id__exact>/graph/', self.admin_site.admin_view(self.hour_graph_view),
                  name='hour_graph'),
         ]
         return employee_urls + urls
 
     def hour_graph_view(self, request, *args, **kwargs):
+        """
+
+        @param request:
+        @param args:
+        @param kwargs:
+        @return:
+        """
         filter_form = FilterForm(initial={
             'project_hour__date__gte': request.GET.get('project_hour__date__gte', ''),
             'project_hour__date__lte': request.GET.get('project_hour__date__lte', '')
@@ -40,6 +51,13 @@ class EmployeeExtraUrls(admin.ModelAdmin):
         return TemplateResponse(request, "admin/employee/hour_graph.html", context)
 
     def _get_chart_data(self, request, *args, **kwargs):
+        """
+
+        @param request:
+        @param args:
+        @param kwargs:
+        @return:
+        """
         chart = {'label': "Weekly View", 'total_hour': 0,
                  'labels': [], 'data': [], }
 
@@ -54,3 +72,61 @@ class EmployeeExtraUrls(admin.ModelAdmin):
             chart['total_hour'] += employee_hour['hours']
 
         return chart
+
+    def all_employee_hour_graph_view(self, request):
+        """
+
+        @param request:
+        @return:
+        """
+        if not request.user.is_superuser:
+            raise PermissionDenied
+        print(request.GET.getlist('id__in[]'))
+        employee_filter = {
+            'active': True,
+            'manager': False,
+            'user__is_superuser': False
+        }
+        if len(request.GET.getlist('id__in[]')) > 0:
+            employee_filter['id__in'] = request.GET.getlist('id__in[]')
+        employees = Employee.objects.filter(**employee_filter).all()
+        dataset = self._get_all_employee_dataset(employees)
+        context = dict(
+            self.admin_site.each_context(request),
+            employees=employees,
+            employees_array=list(employees.values_list('full_name', flat=True)),
+            dataset=dataset
+        )
+        return TemplateResponse(request, "admin/employee/all_employee_hour_graph.html", context)
+
+    def _get_all_employee_dataset(self, employees):
+        """
+
+        @param employees:
+        @return:
+        """
+        dataset = []
+        for project in Project.objects.filter(active=True).all():
+            color = f'rgb{(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}'
+            project_hour = {
+                'label': project.title,
+                'backgroundColor': color,
+                'borderColor': color,
+                'data': self._get_employee_project_hour(project, employees)
+            }
+            dataset.append(project_hour)
+        return dataset
+
+    def _get_employee_project_hour(self, project, employees):
+        """
+
+        @param project:
+        @param employees:
+        @return:
+        """
+        employee_hours = []
+        for employee in employees:
+            total_hour = employee.employeeprojecthour_set.filter(employee=employee).aggregate(Sum('hours'))[
+                'hours__sum']
+            employee_hours.append(total_hour if total_hour is not None else 0)
+        return employee_hours
