06a755c3e93291d10b40ca6e6c73e3888070de7a
kmrifat <kmrifat@gmail.com>
2021-08-04 14:10:43 +0600

    added bank name in salary disbursment

--- src/account/admin/salary_disbursement.py
+++ src/account/admin/salary_disbursement.py
@@ -1,16 +1,28 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.forms.models import ModelChoiceIteratorValue
 
 from account.models import SalaryDisbursement
 from employee.models import Employee
 
 
+class CustomFilteredSelect(FilteredSelectMultiple):
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option_dict = super(CustomFilteredSelect, self).create_option(name, value, label, selected, index,
+                                                                      subindex=None, attrs=None)
+
+        value = ModelChoiceIteratorValue(value, instance=name)
+        employee = Employee.objects.get(pk=value.__str__())
+        option_dict['label'] = f'{label} | {employee.default_bank}'
+        return option_dict
+
+
 class SalaryDisbursementForm(forms.ModelForm):
-    queryset = Employee.objects.filter(active=True).all()
+    queryset = Employee.objects.filter(active=True, user__is_superuser=False).all()
     employee = forms.ModelMultipleChoiceField(
         queryset=queryset,
-        widget=FilteredSelectMultiple(verbose_name='employee', is_stacked=False)
+        widget=CustomFilteredSelect(verbose_name='employee', is_stacked=False),
     )
 
     class Meta:
