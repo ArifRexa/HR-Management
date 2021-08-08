d9fc138af89ccb6cb6c790e8c9232248fd1729eb
kmrifat <kmrifat@gmail.com>
2021-08-08 14:29:54 +0600

    bug fix on salry incrment and permanent

--- src/employee/management/commands/permanent_notifi.py
+++ src/employee/management/commands/permanent_notifi.py
@@ -12,7 +12,8 @@ class Command(BaseCommand):
         print(datetime.datetime.now() - timedelta(days=80))
         employees = Employee.objects.filter(
             joining_date__exact=datetime.datetime.now() - timedelta(days=80),
-            permanent_date__isnull=True
+            permanent_date__isnull=True,
+            active=True
         ).all()
         for employee in employees:
             print(f"{employee.full_name} has joined at {employee.joining_date}")
