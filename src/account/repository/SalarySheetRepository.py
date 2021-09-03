923bf5a92cef4a2a45f7d685f10ad2d1bb803993
kmrifat <kmrifat@gmail.com>
2021-09-03 21:23:29 +0600

    bug fix on salary sheet

--- src/account/repository/SalarySheetRepository.py
+++ src/account/repository/SalarySheetRepository.py
@@ -60,6 +60,8 @@ class SalarySheetRepository:
         self.__employee_current_salary = employee.salaryhistory_set.filter(
             active_from__lte=salary_sheet.date.replace(day=1) - timedelta(days=1)
         ).last()
+        if self.__employee_current_salary is None:
+            self.__employee_current_salary = employee.current_salary
         employee_salary, created = EmployeeSalary.objects.get_or_create(
             employee=employee, salary_sheet=salary_sheet,
             defaults={'net_salary': 0, 'gross_salary': 0}
