307725addd2ad4b7abc7a9ecb11075cd1cee7e42
kmrifat <kmrifat@gmail.com>
2021-09-10 18:49:17 +0600

    exam reminder sking ok

--- src/job_board/management/commands/exam_reminder.py
+++ src/job_board/management/commands/exam_reminder.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
         for candidate_assessment in candidate_assessments:
             subject = f'{candidate_assessment.candidate_job.candidate.full_name},' \
                       f' {candidate_assessment.candidate_job.job.title} || Mediusware Ltd.'
-            from_email, to = 'no-reply@mediusware.com', candidate_assessment.candidate_job.candidate.email
+            from_email, to = 'Mediusware Ltd. <hr@mediusware.com>', candidate_assessment.candidate_job.candidate.email
             html_template = get_template('mail/exam/reminder.html')
             html_content = html_template.render({
                 'candidate_assessment': candidate_assessment,
