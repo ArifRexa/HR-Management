cb3130c15082deefda57dac560741b44f96304ce
shaheen2013 <coredeveloper.2013@gmail.com>
2021-10-04 13:23:20 +0600

    stock list re-order

--- src/asset_management/admin/stock_admin.py
+++ src/asset_management/admin/stock_admin.py
@@ -24,7 +24,7 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
     search_fields = ('title', 'product', 'type')
-    list_display = ('date', 'product', 'get_quantity', 'employee', 'type')
+    list_display = ('date', 'employee', 'product', 'get_quantity', 'note', 'type')
     list_filter = ('type', 'product')
     date_hierarchy = 'date'
 
