9d9f9ac9ea111417c1c813cf248c265a33f11c57
shaheen2013 <coredeveloper.2013@gmail.com>
2021-10-25 13:23:54 +0600

    bug fix

--- src/employee/templatetags/employee_helper.py
+++ src/employee/templatetags/employee_helper.py
@@ -10,12 +10,12 @@ register = template.Library()
 def get_available_leave(employee: Employee, leave_type: str):
     available_leave = 0
     get_leave_by_type = getattr(employee.leave_management, leave_type)
+    print(leave_type, get_leave_by_type)
     if employee.permanent_date:
         total_days_of_permanent = (timezone.now().replace(month=12, day=31).date() - employee.permanent_date).days
         month_of_permanent = round(total_days_of_permanent / 30)
-        print(month_of_permanent)
         if month_of_permanent < 12:
-            available_leave = (month_of_permanent / get_leave_by_type) * get_leave_by_type
+            available_leave = (month_of_permanent * get_leave_by_type) / 12
         else:
             available_leave = get_leave_by_type
-    return available_leave
+    return round(available_leave)
