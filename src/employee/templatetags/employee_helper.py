493202d0f36960b13ce249c0c0ba786fc5b769b0
shaheen2013 <coredeveloper.2013@gmail.com>
2021-11-05 19:22:41 +0600

    NOC formating added bold tag, available leave added

--- src/employee/templatetags/employee_helper.py
+++ src/employee/templatetags/employee_helper.py
@@ -11,8 +11,11 @@ def get_available_leave(employee: Employee, leave_type: str):
     available_leave = 0
     get_leave_by_type = getattr(employee.leave_management, leave_type)
     print(leave_type, get_leave_by_type)
-    if employee.permanent_date:
-        total_days_of_permanent = (timezone.now().replace(month=12, day=31).date() - employee.permanent_date).days
+    if employee.resignation_date:
+        if employee.approved_resignation:
+            total_days_of_permanent = (employee.resignation_date - employee.permanent_date).days
+        else:
+            total_days_of_permanent = (timezone.now().replace(month=12, day=31).date() - employee.permanent_date).days
         month_of_permanent = round(total_days_of_permanent / 30)
         if month_of_permanent < 12:
             available_leave = (month_of_permanent * get_leave_by_type) / 12
