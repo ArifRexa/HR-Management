8dec5b16ad82d831fa08e99eb069215a693b6261
kmrifat <kmrifat@gmail.com>
2022-06-23 19:17:35 +0600

    added income

--- src/account/admin/income.py
+++ src/account/admin/income.py
@@ -14,7 +14,7 @@ from account.services.balance import BalanceSummery
 
 @admin.register(Income)
 class IncomeAdmin(admin.ModelAdmin):
-    list_display = ('project', 'date', 'hours', 'hour_rate', 'payment_details', 'status_col')
+    list_display = ('project', 'date', 'hours', 'hour_rate', 'convert_rate', 'payment_details', 'status_col')
     date_hierarchy = 'date'
     readonly_fields = ('payment',)
     list_filter = ('status', 'project', 'hour_rate', 'date')
