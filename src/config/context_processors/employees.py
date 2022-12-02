9f063e12ae6da9bc88714818e5c47cc22a35694e
K M Rifat ul alom <kmrifat@gmail.com>
2022-12-02 19:22:53 +0600

    added employee online status on dashabord

--- src/config/context_processors/employees.py
+++ src/config/context_processors/employees.py
@@ -5,12 +5,15 @@ from employee.models import EmployeeOnline
 
 def formal_summery(request):
     employee_formal_summery = EmployeeNearbySummery()
+    employee_offline = EmployeeOnline.objects.filter(employee__active=True).order_by('active',
+                                                                                     'employee__full_name').all()
     return {
         "leaves": employee_formal_summery.employee_leave_nearby,
         "birthdays": employee_formal_summery.birthdays,
         "increments": employee_formal_summery.increments,
         "permanents": employee_formal_summery.permanents,
-        "anniversaries": employee_formal_summery.anniversaries
+        "anniversaries": employee_formal_summery.anniversaries,
+        'employee_offline': employee_offline
     }
 
 
