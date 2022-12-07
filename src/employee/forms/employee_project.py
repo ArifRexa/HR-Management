386779fd2614ed23e75a6519d89def4b6b7d24aa
zayed <abdullahzayed01@gmail.com>
2022-12-07 16:30:03 +0600

    working: muti select dropdown

--- src/employee/forms/employee_project.py
+++ src/employee/forms/employee_project.py
@@ -7,3 +7,7 @@ class EmployeeProjectForm(forms.ModelForm):
     class Meta:
         model = EmployeeProject
         fields = ['project']
+    
+    def __init__(self, *args, **kwargs):
+        super(EmployeeProjectForm, self).__init__(*args, **kwargs)
+        #self.fields['project'].widget.attrs.update({'hidden': 'hidden'})
