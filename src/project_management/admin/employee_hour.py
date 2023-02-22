2b631a672f4d245d8b7b70458063ac0b93ff88c1
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-02-22 14:21:09 +0600

    fixed: text in action bar in daily update

--- src/project_management/admin/employee_hour.py
+++ src/project_management/admin/employee_hour.py
@@ -224,12 +224,12 @@ class DailyProjectUpdateAdmin(admin.ModelAdmin):
     def update_status_approve(self, request, queryset):
         queryset.update(status='approved')
 
-    update_status_approve.short_description = "Approved selected status"
+    update_status_approve.short_description = "Approve selected daily project updates"
 
     def update_status_pending(self, request, queryset):
         queryset.update(status='pending')
 
-    update_status_pending.short_description = "Pending selected status"
+    update_status_pending.short_description = "Pending selected status daily project updates"
 
     def has_delete_permission(self, request, obj=None):
         permitted = super().has_delete_permission(request, obj=obj)
