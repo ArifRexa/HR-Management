ea885df37cabab6407ae352237add0868a5034de
shaheen - Sahadat <coredeveloper.2013@gmail.com>
2023-03-02 21:36:25 +0600

    change config menu

--- src/employee/admin/config.py
+++ src/employee/admin/config.py
@@ -4,4 +4,6 @@ from employee.models.config import Config
 
 @admin.register(Config)
 class ConfigAdmin(admin.ModelAdmin):
-    pass 
+    
+    def has_module_permission(self, request):
+        return False
