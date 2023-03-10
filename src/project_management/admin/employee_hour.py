5c200226fbcccefe3d6a776dc473cf8f559eabea
sahadatpro <sahadatsays@gmail.com>
2023-03-10 23:16:27 +0600

    remove

--- src/project_management/admin/employee_hour.py
+++ src/project_management/admin/employee_hour.py
@@ -342,8 +342,7 @@ class ProjectUpdateGroupByEmployeeAdmin(admin.ModelAdmin):
 
         info = self.model._meta.app_label, self.model._meta.model_name
         custome_urls = [
-            path("admin/", wrap(self.changelist_view), name="%s_%s_changelist" % info),
-            path("", self.custom_changelist_view, name='groupbyemp_changelist_view'),
+            path("admin/", wrap(self.changelist_view), name="%s_%s_changelist" % info)
         ]
         return custome_urls + urls
 
