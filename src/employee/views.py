2e15098748712d4ceab9bbacb131a0ceab6dda45
sahadatpro <sahadatsays@gmail.com>
2023-03-13 14:14:57 +0600

    rest url remove

--- src/employee/views.py
+++ src/employee/views.py
@@ -14,9 +14,6 @@ from config.settings import employee_ids as management_ids
 # white_listed_ips = ['103.180.244.213', '127.0.0.1', '134.209.155.127', '45.248.149.252']
 from django.http import HttpResponse
 
-def test(request):
-    return HttpResponse('Hello Test')
-
 @white_listed_ip_check
 @require_http_methods(['POST', 'GET'])
 @login_required(login_url='/admin/login/')
