c355ebc1465e3d874289b9cc39ee63ac815e7e18
sahadatpro <sahadatsays@gmail.com>
2023-04-12 20:25:11 +0600

    change Daily project update

--- src/project_management/admin/employee_hour.py
+++ src/project_management/admin/employee_hour.py
@@ -112,178 +112,182 @@ class DailyProjectUpdateDocumentAdmin(admin.TabularInline):
 
 @admin.register(DailyProjectUpdate)
 class DailyProjectUpdateAdmin(admin.ModelAdmin):
-    inlines = [DailyProjectUpdateDocumentAdmin, ]
-    list_display = ('get_date', 'employee', 'project',
-                    'get_hours', 'history', 'get_update', 'manager', 'status_col')
-    list_filter = ('status', 'project', 'employee', 'manager', )
-    search_fields = ('employee__full_name', 'project__title',
-                     'manager__full_name', )
-    date_hierarchy = 'created_at'
-    autocomplete_fields = ('employee', 'project', )
-    change_list_template = 'admin/total_employee_hour.html'
-    readonly_fields = ['status', 'created_at']
-    actions = ['update_status_approve', 'update_status_pending']
-    fieldsets = (
-        ('Standard info', {
-            'fields': ('created_at', 'employee', 'manager', 'project', 'hours', 'update', 'status')
-        }),
-    )
+    list_display = ( 'employee', 'project')
 
-    class Media:
-        css = {
-            'all': ('css/list.css',)
-        }
-        js = ('js/list.js',)
+# @admin.register(DailyProjectUpdate)
+# class DailyProjectUpdateAdmin(admin.ModelAdmin):
+#     inlines = [DailyProjectUpdateDocumentAdmin, ]
+#     list_display = ('get_date', 'employee', 'project',
+#                     'get_hours', 'history', 'get_update', 'manager', 'status_col')
+#     list_filter = ('status', 'project', 'employee', 'manager', )
+#     search_fields = ('employee__full_name', 'project__title',
+#                      'manager__full_name', )
+#     date_hierarchy = 'created_at'
+#     autocomplete_fields = ('employee', 'project', )
+#     change_list_template = 'admin/total_employee_hour.html'
+#     readonly_fields = ['status', 'created_at']
+#     actions = ['update_status_approve', 'update_status_pending']
+#     fieldsets = (
+#         ('Standard info', {
+#             'fields': ('created_at', 'employee', 'manager', 'project', 'hours', 'update', 'status')
+#         }),
+#     )
 
-    def get_readonly_fields(self, request, obj=None):
-        if request.user.is_superuser:
-            return ['created_at',]
+#     class Media:
+#         css = {
+#             'all': ('css/list.css',)
+#         }
+#         js = ('js/list.js',)
 
-        if obj:
-            if request.user.employee.manager:
-                if obj.manager != obj.employee and obj.manager == request.user.employee:
-                    return ['created_at', 'employee', 'manager', 'project', 'update']
-                else:
-                    return ['created_at',]
-            if request.user.employee == obj.employee:
-                if obj.status == 'approved':
-                    return ['created_at', 'employee', 'manager', 'project', 'update', 'hours', 'status']
-                else:
-                    return self.readonly_fields
-            else:
-                return self.readonly_fields
+#     def get_readonly_fields(self, request, obj=None):
+#         if request.user.is_superuser:
+#             return ['created_at',]
 
-        else:
-            if request.user.employee.manager:
-                return ['created_at']
-            else:
-                return self.readonly_fields
-    def history(self, obj):
-        # historyData = ""
-        # if obj.history is not None:
-        #     for history in obj.history.order_by('-created_at'):
-        #         historyData += f"{history.hours}"
-        #         if history != obj.history.order_by('-created_at').last():
-        #              historyData += f" > "
-        #     return format_html(historyData)
+#         if obj:
+#             if request.user.employee.manager:
+#                 if obj.manager != obj.employee and obj.manager == request.user.employee:
+#                     return ['created_at', 'employee', 'manager', 'project', 'update']
+#                 else:
+#                     return ['created_at',]
+#             if request.user.employee == obj.employee:
+#                 if obj.status == 'approved':
+#                     return ['created_at', 'employee', 'manager', 'project', 'update', 'hours', 'status']
+#                 else:
+#                     return self.readonly_fields
+#             else:
+#                 return self.readonly_fields
 
-        return 'No changes'
+#         else:
+#             if request.user.employee.manager:
+#                 return ['created_at']
+#             else:
+#                 return self.readonly_fields
+#     def history(self, obj):
+#         historyData = ""
+#         if obj.history is not None:
+#             for history in obj.history.order_by('-created_at'):
+#                 historyData += f"{history.hours}"
+#                 if history != obj.history.order_by('-created_at').last():
+#                      historyData += f" > "
+#             return format_html(historyData)
 
-    @admin.display(description="Date", ordering='created_at')
-    def get_date(self, obj):
-        return obj.created_at
+#         return 'No changes'
 
-    @admin.display(description="Update")
-    def get_update(self, obj):
-        html_template = get_template(
-            'admin/project_management/list/col_dailyupdate.html')
-        html_content = html_template.render({
-            'update': obj.update,
-        })
-        return format_html(html_content)
+#     @admin.display(description="Date", ordering='created_at')
+#     def get_date(self, obj):
+#         return obj.created_at
 
-    @admin.display(description="Hours", ordering='hours')
-    def get_hours(self, obj):
-        custom_style = ''
-        if obj.hours <= 4:
-            custom_style = ' style="color:red; font-weight: bold;"'
-        html_content = f'<span{custom_style}>{obj.hours}</span>'
-        return format_html(html_content)
+#     @admin.display(description="Update")
+#     def get_update(self, obj):
+#         html_template = get_template(
+#             'admin/project_management/list/col_dailyupdate.html')
+#         html_content = html_template.render({
+#             'update': obj.update,
+#         })
+#         return format_html(html_content)
 
-    def changelist_view(self, request, extra_context=None):
-        filter_form = DailyUpdateFilterForm(initial={
-            'created_at__date__gte': request.GET.get('created_at__date__gte', timezone.now().date()-datetime.timedelta(days=7)),
-            'created_at__date__lte': request.GET.get('created_at__date__lte', timezone.now().date())
-        })
-        my_context = {
-            'total': self.get_total_hour(request),
-            'filter_form': filter_form,
-        }
-        return super(DailyProjectUpdateAdmin, self).changelist_view(request, extra_context=my_context)
+#     @admin.display(description="Hours", ordering='hours')
+#     def get_hours(self, obj):
+#         custom_style = ''
+#         if obj.hours <= 4:
+#             custom_style = ' style="color:red; font-weight: bold;"'
+#         html_content = f'<span{custom_style}>{obj.hours}</span>'
+#         return format_html(html_content)
 
-    def get_total_hour(self, request):
-        qs = self.get_queryset(request).filter(
-            **simple_request_filter(request))
-        return qs.aggregate(tot=Sum('hours'))['tot']
+#     def changelist_view(self, request, extra_context=None):
+#         filter_form = DailyUpdateFilterForm(initial={
+#             'created_at__date__gte': request.GET.get('created_at__date__gte', timezone.now().date()-datetime.timedelta(days=7)),
+#             'created_at__date__lte': request.GET.get('created_at__date__lte', timezone.now().date())
+#         })
+#         my_context = {
+#             'total': self.get_total_hour(request),
+#             'filter_form': filter_form,
+#         }
+#         return super(DailyProjectUpdateAdmin, self).changelist_view(request, extra_context=my_context)
 
-    def get_queryset(self, request):
-        query_set = super(DailyProjectUpdateAdmin, self).get_queryset(request)
+#     def get_total_hour(self, request):
+#         qs = self.get_queryset(request).filter(
+#             **simple_request_filter(request))
+#         return qs.aggregate(tot=Sum('hours'))['tot']
 
-        if not request.user.is_superuser and not request.user.has_perm("project_management.see_all_employee_update"):
-            if request.user.employee.manager:
-                query_set = query_set.filter(
-                    Q(manager=request.user.employee) |
-                    Q(employee=request.user.employee),
-                )
-            else:
-                query_set = query_set.filter(employee=request.user.employee)
+#     def get_queryset(self, request):
+#         query_set = super(DailyProjectUpdateAdmin, self).get_queryset(request)
 
-        return query_set
+#         if not request.user.is_superuser and not request.user.has_perm("project_management.see_all_employee_update"):
+#             if request.user.employee.manager:
+#                 query_set = query_set.filter(
+#                     Q(manager=request.user.employee) |
+#                     Q(employee=request.user.employee),
+#                 )
+#             else:
+#                 query_set = query_set.filter(employee=request.user.employee)
 
-    def get_list_filter(self, request):
-        filters = list(super(DailyProjectUpdateAdmin,
-                       self).get_list_filter(request))
-        if not request.user.is_superuser and not request.user.has_perm("project_management.see_all_employee_update"):
-            if 'employee' in filters:
-                filters.remove('employee')
-        return filters
+#         return query_set
 
-    def has_change_permission(self, request, obj=None):
-        permitted = super().has_change_permission(request, obj=obj)
-        if not request.user.is_superuser \
-            and obj \
-                and obj.employee != request.user.employee \
-        and obj.manager != request.user.employee:
-            permitted = False
-        return permitted
+#     def get_list_filter(self, request):
+#         filters = list(super(DailyProjectUpdateAdmin,
+#                        self).get_list_filter(request))
+#         if not request.user.is_superuser and not request.user.has_perm("project_management.see_all_employee_update"):
+#             if 'employee' in filters:
+#                 filters.remove('employee')
+#         return filters
 
-    @admin.action(description='Status')
-    def status_col(self, obj):
-        color = 'red'
-        if obj.status == 'approved':
-            color = 'green'
-        return format_html(
-            f'<b style="color: {color}">{obj.get_status_display()}</b>'
-        )
+#     def has_change_permission(self, request, obj=None):
+#         permitted = super().has_change_permission(request, obj=obj)
+#         if not request.user.is_superuser \
+#             and obj \
+#                 and obj.employee != request.user.employee \
+#         and obj.manager != request.user.employee:
+#             permitted = False
+#         return permitted
 
-    @admin.action(description='Approve selected status daily project updates')
-    def update_status_approve(modeladmin, request, queryset):
-        if request.user.is_superuser:
-            for obj in queryset:
-                obj.status = 'approved'
-                obj.save()
-        elif request.user.employee.manager:
-            queryset = queryset.filter(manager_id=request.user.employee.id)
-            for obj in queryset:
-                obj.status = 'approved'
-                obj.save()
+#     @admin.action(description='Status')
+#     def status_col(self, obj):
+#         color = 'red'
+#         if obj.status == 'approved':
+#             color = 'green'
+#         return format_html(
+#             f'<b style="color: {color}">{obj.get_status_display()}</b>'
+#         )
 
-    @admin.action(description='Pending selected status daily project updates')
-    def update_status_pending(modeladmin, request, queryset):
-        if request.user.is_superuser:
-            for obj in queryset:
-                obj.status = 'pending'
-                obj.save()
-        elif request.user.employee.manager:
-            queryset = queryset.filter(manager_id=request.user.employee.id)
-            for obj in queryset:
-                obj.status = 'pending'
-                obj.save()
-        # return
+#     @admin.action(description='Approve selected status daily project updates')
+#     def update_status_approve(modeladmin, request, queryset):
+#         if request.user.is_superuser:
+#             for obj in queryset:
+#                 obj.status = 'approved'
+#                 obj.save()
+#         elif request.user.employee.manager:
+#             queryset = queryset.filter(manager_id=request.user.employee.id)
+#             for obj in queryset:
+#                 obj.status = 'approved'
+#                 obj.save()
 
-    def has_delete_permission(self, request, obj=None):
-        permitted = super().has_delete_permission(request, obj=obj)
-        if not request.user.is_superuser and obj and obj.employee != request.user.employee:
-            permitted = False
-        return permitted
+#     @admin.action(description='Pending selected status daily project updates')
+#     def update_status_pending(modeladmin, request, queryset):
+#         if request.user.is_superuser:
+#             for obj in queryset:
+#                 obj.status = 'pending'
+#                 obj.save()
+#         elif request.user.employee.manager:
+#             queryset = queryset.filter(manager_id=request.user.employee.id)
+#             for obj in queryset:
+#                 obj.status = 'pending'
+#                 obj.save()
+#         # return
+
+#     def has_delete_permission(self, request, obj=None):
+#         permitted = super().has_delete_permission(request, obj=obj)
+#         if not request.user.is_superuser and obj and obj.employee != request.user.employee:
+#             permitted = False
+#         return permitted
     
-    def save_model(self, request, obj, form, change) -> None:
-        super().save_model(request, obj, form, change)
+#     def save_model(self, request, obj, form, change) -> None:
+#         super().save_model(request, obj, form, change)
         
-        if change == False:
-            return DailyProjectUpdateHistory.objects.create(hours=request.POST.get('hours'), daily_update=obj)
+#         if change == False:
+#             return DailyProjectUpdateHistory.objects.create(hours=request.POST.get('hours'), daily_update=obj)
             
-        requested_hours = float(request.POST.get('hours'))
-        if requested_hours != obj.hours or obj.created_by is not request.user:
-            return DailyProjectUpdateHistory.objects.create(hours=request.POST.get('hours'), daily_update=obj)
+#         requested_hours = float(request.POST.get('hours'))
+#         if requested_hours != obj.hours or obj.created_by is not request.user:
+#             return DailyProjectUpdateHistory.objects.create(hours=request.POST.get('hours'), daily_update=obj)
         
