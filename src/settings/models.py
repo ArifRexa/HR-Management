da983450226df9a520348db19c9ac2dd7890499a
xaadu <abdullahzayed01@gmail.com>
2023-09-11 20:48:05 +0600

    [ ADD ] separate food allowance menu

--- src/settings/models.py
+++ src/settings/models.py
@@ -31,19 +31,21 @@ class PayScale(TimeStampMixin, AuthorMixin):
     house_allowance = models.FloatField()
     medical_allowance = models.FloatField()
     net_payable = models.FloatField()
-    provision_period = models.IntegerField(help_text='Month')
-    increment_period = models.IntegerField(help_text='increment month count')
-    increment_rate = models.FloatField(help_text='In percentage')
+    provision_period = models.IntegerField(help_text="Month")
+    increment_period = models.IntegerField(help_text="increment month count")
+    increment_rate = models.FloatField(help_text="In percentage")
     leave_in_cash_medical = models.FloatField(
-        help_text='Medical Leave in cash, your submitted value will count as % percentage. '
-                  'It will automatically calculate to the employee salary sheet on year closing',
-        verbose_name='Leave in Cash (Medical)',
-        default=0.0)
+        help_text="Medical Leave in cash, your submitted value will count as % percentage. "
+        "It will automatically calculate to the employee salary sheet on year closing",
+        verbose_name="Leave in Cash (Medical)",
+        default=0.0,
+    )
     leave_in_cash_casual = models.FloatField(
-        help_text='Casual leave in cash, your submitted value will count as % percentage. '
-                  'It will automatically calculate to the employee salary sheet on year closing',
-        verbose_name='Leave in Cash (Casual)',
-        default=0.0)
+        help_text="Casual leave in cash, your submitted value will count as % percentage. "
+        "It will automatically calculate to the employee salary sheet on year closing",
+        verbose_name="Leave in Cash (Casual)",
+        default=0.0,
+    )
 
     def __str__(self):
         return self.title
@@ -67,7 +69,9 @@ class PublicHoliday(TimeStampMixin, AuthorMixin):
 
 
 class PublicHolidayDate(TimeStampMixin):
-    public_holiday = models.ForeignKey(PublicHoliday, on_delete=models.CASCADE, related_name='public_holiday')
+    public_holiday = models.ForeignKey(
+        PublicHoliday, on_delete=models.CASCADE, related_name="public_holiday"
+    )
     date = models.DateField()
 
 
@@ -94,14 +98,43 @@ class OpenLetter(TimeStampMixin):
     message = models.TextField()
 
 
-
 class Announcement(TimeStampMixin, AuthorMixin):
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
-    
+
     rank = models.SmallIntegerField(default=0)
-    
+
     description = models.TextField()
 
     is_active = models.BooleanField(default=True)
 
+
+# class FoodAllowanceExclusion(TimeStampMixin, AuthorMixin):
+#     date = models.DateField()
+#     number = models.IntegerField(help_text="Number of Food Allowance to be excluded")
+
+
+# class EmployeeFoodAllowanceExclusion(TimeStampMixin, AuthorMixin):
+#     foodallowance = models.ForeignKey(
+#         to=FoodAllowanceExclusion,
+#         on_delete=models.CASCADE,
+#     )
+#     employee = models.ForeignKey(
+#         to="employee.Employee",
+#         on_delete=models.CASCADE,
+#         limit_choices_to={"active": True},
+#     )
+#     number = models.IntegerField()
+
+
+class EmployeeFoodAllowance(TimeStampMixin, AuthorMixin):
+    employee = models.ForeignKey(
+        to="employee.Employee",
+        on_delete=models.CASCADE,
+        limit_choices_to={"active": True},
+    )
+    date = models.DateField()
+    amount = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.employee.full_name} | {self.amount} | {self.date.strftime('%B %d, %Y')}"
