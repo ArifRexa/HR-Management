d32ffb6d4e1cf71150f18ba285141aedb8be99b0
mmrubayet <rubayet.wrk@gmail.com>
2023-10-30 20:14:11 +0600

    remove app ready func

--- src/client_management/apps.py
+++ src/client_management/apps.py
@@ -4,10 +4,3 @@ from django.apps import AppConfig
 class ClientManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'client_management'
-
-    def ready(self):
-        from project_management.models import Project, create_project_identifier
-
-        for project in Project.objects.all():
-            project.identifier = create_project_identifier()
-            project.save()
