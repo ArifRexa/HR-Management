1ad3618163fcbd1370a5f7b12a9dfba5f309c382
shaheen2013 <coredeveloper.2013@gmail.com>
2023-10-30 19:43:44 +0600

    conflict resolved, removed ready

--- src/client_management/apps.py
+++ src/client_management/apps.py
@@ -5,9 +5,9 @@ class ClientManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'client_management'
 
-    def ready(self):
-        from project_management.models import Project, create_project_identifier
-
-        for project in Project.objects.all():
-            project.identifier = create_project_identifier()
-            project.save()
+    # def ready(self):
+    #     from project_management.models import Project, create_project_identifier
+    #
+    #     for project in Project.objects.all():
+    #         project.identifier = create_project_identifier()
+    #         project.save()
