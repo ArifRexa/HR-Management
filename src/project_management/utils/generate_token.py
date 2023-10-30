352f0f76ff2418d59950ae651cf13523f8db17ae
mmrubayet <rubayet.wrk@gmail.com>
2023-10-30 21:45:08 +0600

    project identifier generator

--- src/project_management/utils/generate_token.py
+++ src/project_management/utils/generate_token.py
@@ -7,9 +7,17 @@ def generate_token():
     for project in projects:
         ProjectToken.objects.update_or_create(
             project_id=project.id,
-            defaults= {
+            defaults={
                 'token': uuid4()
             }
         )
     # Safety
     ProjectToken.objects.filter(project__active=False).delete()
+
+
+def generate_identifier():
+    projects = Project.objects.all()
+    for project in projects:
+        if project.identifier is None:
+            project.identifier = uuid4()
+            project.save()
