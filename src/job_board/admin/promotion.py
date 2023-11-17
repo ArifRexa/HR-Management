8524e061dd404f773832a38c118f4f34c0852ef8
borhanst <mdborhan.st@gmail.com>
2023-11-17 12:59:05 +0600

    job_board menu move to others

--- src/job_board/admin/promotion.py
+++ src/job_board/admin/promotion.py
@@ -7,3 +7,6 @@ from job_board.models.promotion import SMSPromotion
 class SMSPromotionAdmin(admin.ModelAdmin):
     list_display = ('title', 'sms_body', 'is_default')
     list_editable = ('is_default',)
+
+    def has_module_permission(self, request):
+        return False
