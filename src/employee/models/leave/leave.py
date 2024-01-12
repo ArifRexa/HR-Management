50dd11facab64c3ab0b35deb8d2c3f50811050df
Partho Debnath <parthodebnath60@gmail.com>
2024-01-12 17:27:18 +0600

    [employee] Employees can not apply any leave application after 06:00 PM, functionality has been added

--- src/employee/models/leave/leave.py
+++ src/employee/models/leave/leave.py
@@ -7,12 +7,18 @@ from config.model.AuthorMixin import AuthorMixin
 from config.model.TimeStampMixin import TimeStampMixin
 from employee.models.leave.LeaveMixin import LeaveMixin
 from employee.models.employee import Employee
+
+
 # TODO : leave calculation by permanent date
 # TODO : leave in cash in every january
 class Leave(TimeStampMixin, AuthorMixin, LeaveMixin):
     message = models.TextField(validators=[MinLengthValidator(150)])
-    status_changed_by = models.ForeignKey(User, limit_choices_to={'is_superuser': True}, null=True,
-                                          on_delete=models.RESTRICT)
+    status_changed_by = models.ForeignKey(
+        User,
+        limit_choices_to={"is_superuser": True},
+        null=True,
+        on_delete=models.RESTRICT,
+    )
     status_changed_at = models.DateField(null=True)
 
     @property
@@ -24,21 +30,24 @@ class Leave(TimeStampMixin, AuthorMixin, LeaveMixin):
 
     class Meta:
         permissions = (
-            ("can_approve_leave_applications", "Can able to approve leave applications"),
+            (
+                "can_approve_leave_applications",
+                "Can able to approve leave applications",
+            ),
         )
 
 
 class LeaveAttachment(TimeStampMixin, AuthorMixin):
     leave = models.ForeignKey(Leave, on_delete=models.CASCADE)
-    attachment = models.FileField(help_text='Image , PDF or Docx file ')
+    attachment = models.FileField(help_text="Image , PDF or Docx file ")
 
 
 class LeaveManagement(TimeStampMixin):
     LEAVE_STATUS = (
-        ('pending', '⏳ Pending'),
-        ('approved', '\u2705 Approved'),
-        ('rejected', '⛔ Rejected'),
-        ('need_action', '◌ Need Further Discussion'),
+        ("pending", "⏳ Pending"),
+        ("approved", "\u2705 Approved"),
+        ("rejected", "⛔ Rejected"),
+        ("need_action", "◌ Need Further Discussion"),
     )
     leave = models.ForeignKey(Leave, on_delete=models.CASCADE)
     manager = models.ForeignKey(
@@ -46,14 +55,14 @@ class LeaveManagement(TimeStampMixin):
         on_delete=models.SET_NULL,
         null=True,
         limit_choices_to=(Q(active=True) & (Q(manager=True) | Q(lead=True))),
-        related_name='leave_management_manager'
+        related_name="leave_management_manager",
     )
-    status = models.CharField(max_length=20, choices=LEAVE_STATUS, default='pending')
+    status = models.CharField(max_length=20, choices=LEAVE_STATUS, default="pending")
     approval_time = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.leave.employee.full_name} : {self.manager.full_name}"
-    class Meta:
-        verbose_name = 'Leave Approval'
-        verbose_name_plural = "Leave Approvals"
 
+    class Meta:
+        verbose_name = "Leave Approval"
+        verbose_name_plural = "Leave Approvals"
