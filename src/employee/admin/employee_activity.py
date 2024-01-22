2ad2649f6d4307f70b9c597a73eebf3501fffa8e
Partho Debnath <parthodebnath60@gmail.com>
2024-01-22 14:58:09 +0600

    [employee] Holiday System Disabled

--- src/employee/admin/employee_activity.py
+++ src/employee/admin/employee_activity.py
@@ -137,9 +137,12 @@ class EmployeeAttendanceAdmin(admin.ModelAdmin):
         DEFAULT_EXIT_HOUR = 12 + 8  # 24 Hour time == 9 pm
         DEFAULT_EXIT_TIME = now.replace(hour=DEFAULT_EXIT_HOUR, minute=0, second=0)
 
-        last_x_dates = [(now - datetime.timedelta(i)).date() for i in range(10)]
+        last_x_dates = [
+            (now - datetime.timedelta(i)).date()
+            for i in range(10)
+            if (now - datetime.timedelta(i)).date().strftime("%a") not in ["Sat", "Sun"]
+        ]
         last_x_date = (now - datetime.timedelta(10)).date()
-
         last_month = (now.replace(day=1) - datetime.timedelta(days=1)).date()
 
         emps = (
@@ -224,7 +227,13 @@ class EmployeeAttendanceAdmin(admin.ModelAdmin):
                             employee_is_lead = emp.lead or emp.manager
                             start_time_timeobj = start_time.time()
                             if start_time:
-                                is_late = (employee_is_lead and start_time_timeobj.hour >= 13) or (not employee_is_lead and start_time_timeobj.hour >= 11 and start_time_timeobj.minute >= 30)
+                                is_late = (
+                                    employee_is_lead and start_time_timeobj.hour >= 13
+                                ) or (
+                                    not employee_is_lead
+                                    and start_time_timeobj.hour >= 11
+                                    and start_time_timeobj.minute >= 30
+                                )
                             temp[date].update(
                                 {
                                     "entry_time": start_time.time()
@@ -246,8 +255,8 @@ class EmployeeAttendanceAdmin(admin.ModelAdmin):
                                     "total_time_hour": math.floor(
                                         (inside_time + break_time) / (60 * 60) % 24
                                     ),
-                                    "employee_is_lead":emp.lead or emp.manager,
-                                    "is_late":is_late
+                                    "employee_is_lead": emp.lead or emp.manager,
+                                    "is_late": is_late,
                                 }
                             )
                         break
