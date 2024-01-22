37ded2f53e3ffbbe05508c4550685ca92d3a1e2c
Partho Debnath <parthodebnath60@gmail.com>
2024-01-22 18:05:01 +0600

    [employee] added employee ratting soartable

--- src/employee/admin/employeee_rating_admin.py
+++ src/employee/admin/employeee_rating_admin.py
@@ -8,45 +8,62 @@ from django.utils.html import format_html
 from django.http.request import HttpRequest
 from employee.models.employee_rating_models import EmployeeRating
 
+
 class EmployeeRatingForm(forms.ModelForm):
     model = EmployeeRating
-    fields = '__all__'
+    fields = "__all__"
 
     def clean(self):
         clean_data = super().clean()
         request = self.request
 
-        if request.user.employee.id == self.cleaned_data.get('employee').id:
-            raise forms.ValidationError({'employee': 'You cannot rate yourself. Plesae rate someone other.'})
+        if request.user.employee.id == self.cleaned_data.get("employee").id:
+            raise forms.ValidationError(
+                {"employee": "You cannot rate yourself. Plesae rate someone other."}
+            )
 
-        if clean_data.get('employee'):
+        if clean_data.get("employee"):
             before_week = datetime.now() - timedelta(days=7)
             is_provided = EmployeeRating.objects.filter(
-                                                        created_at__gt = before_week, 
-                                                        employee = clean_data.get('employee'), 
-                                                        project = clean_data.get('project'),
-                                                        created_by = request.user).exists()
+                created_at__gt=before_week,
+                employee=clean_data.get("employee"),
+                project=clean_data.get("project"),
+                created_by=request.user,
+            ).exists()
             if is_provided and self.instance.id is None:
-                raise forms.ValidationError({'employee': 'You already given the rating. Plesae try again 7 days later.'})
+                raise forms.ValidationError(
+                    {
+                        "employee": "You already given the rating. Plesae try again 7 days later."
+                    }
+                )
 
             if self.instance.id and self.instance.created_at <= before_week:
-                raise ValidationError({"comment": "You can\'t update your rating!"})
+                raise ValidationError({"comment": "You can't update your rating!"})
         return clean_data
-         
+
+
 @admin.register(EmployeeRating)
 class EmployeeRatingAdmin(admin.ModelAdmin):
-    list_display = ['employee', 'rating_by', 'project', 'show_score', 'comment', 'created_at']
-    date_hierarchy = 'created_at'
-    list_filter = ['employee', 'project']
-    autocomplete_fields = ['employee', 'project']
+    list_display = [
+        "employee",
+        "rating_by",
+        "project",
+        "show_score",
+        "comment",
+        "created_at",
+    ]
+    date_hierarchy = "created_at"
+    list_filter = ["employee", "project"]
+    autocomplete_fields = ["employee", "project"]
     form = EmployeeRatingForm
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        if request.user.has_perm('employee.can_view_all_ratings'):
-            return qs;
+        if request.user.has_perm("employee.can_view_all_ratings"):
+            return qs
         return qs.filter(created_by__id=request.user.id)
 
+    @admin.display(description="Show Score", ordering="score")
     def show_score(self, obj):
         string = f'<strong style="color:green">{obj.score}</strong>'
         if obj.score <= 5:
@@ -57,22 +74,20 @@ class EmployeeRatingAdmin(admin.ModelAdmin):
         delete_or_update_before = datetime.now() + timedelta(days=7)
         if obj is None:
             return False
-        
+
         if obj.created_at > delete_or_update_before:
             return False
         return True
 
     @admin.display(description="Rating By")
     def rating_by(self, obj):
-        return f'{obj.created_by.employee.full_name}'
-    
-    
+        return f"{obj.created_by.employee.full_name}"
+
     def get_form(self, request, obj, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         if obj == None:
-            field = form.base_fields['employee']
+            field = form.base_fields["employee"]
             field.widget.can_add_related = False
             field.widget.can_change_related = False
         form.request = request
         return form
-    
