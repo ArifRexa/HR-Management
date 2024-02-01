becfcff30f0424cc7768ed5bd7944d7a8688cd8a
mdishmam <fatinishmam@gmail.com>
2024-02-01 18:32:04 +0600

    migration erroe resolve

--- src/account/migrations/0110_alter_accountjournal_date.py
+++ src/account/migrations/0110_alter_accountjournal_date.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('account', '0109_alter_accountjournal_date'),
+        # ('account', '0109_alter_accountjournal_date'),
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='accountjournal',
-            name='date',
-            field=models.DateField(null=True),
-        ),
+        # migrations.AlterField(
+        #     model_name='accountjournal',
+        #     name='date',
+        #     field=models.DateField(null=True),
+        # ),
     ]
