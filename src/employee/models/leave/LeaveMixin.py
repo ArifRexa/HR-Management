8496971b21cf747cb60359f1e85d3ec6fb662bf7
MdAkash3135 <mdeaqubmia@gmail.com>
2024-02-05 15:09:30 +0600

    error notification modified

--- src/employee/models/leave/LeaveMixin.py
+++ src/employee/models/leave/LeaveMixin.py
@@ -12,7 +12,7 @@ class LeaveMixin(models.Model):
     MEDICAL = 'medical'
     HALF_DAY = 'half_day'
     APPLIED_TIME_LIMIT = 15
-    APPLIED_ERROR_MSG = "You can not apply any leave application after 06:00 PM for tomorrow."
+    APPLIED_ERROR_MSG = "You can not apply any leave application after 03:00 PM for tomorrow."
     LEAVE_CHOICE = (
         ("casual", "Casual Leave"),
         ("medical", "Medical Leave"),
