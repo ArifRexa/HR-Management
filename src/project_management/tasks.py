96e05b62bdcc847f2629a3a063f040a236f31147
MdAkash3135 <51851591+MdAkash3135@users.noreply.github.com>
2024-02-19 15:17:21 +0600

    resolve a bug

--- src/project_management/tasks.py
+++ src/project_management/tasks.py
@@ -7,7 +7,7 @@ from employee.models.employee import Observation
 def mark_employee_free():
     management.call_command('mark_employee_free')
 
-def delete_new_proj(self, *args, **kwargs):
+def delete_new_proj():
         # Calculate the date two weeks ago
         two_weeks_ago = datetime.now() - timedelta(weeks=2)
         
@@ -15,4 +15,4 @@ def delete_new_proj(self, *args, **kwargs):
         deleted_count, _ = ObservationProject.objects.filter(created_at__lt=two_weeks_ago).delete()
         deleted_employee = Observation.objects.filter(created_at__lt=two_weeks_ago).delete()
         # Output result
-        self.stdout.write(self.style.SUCCESS(f'{deleted_count} {deleted_employee} records deleted successfully'))
+        # self.stdout.write(self.style.SUCCESS(f'{deleted_count} {deleted_employee} records deleted successfully'))
