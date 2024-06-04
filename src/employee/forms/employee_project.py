7a6a17c5c9a891213127f7d9d6b6fc1386a7078b
shaheen2013 <coredeveloper.2013@gmail.com>
2024-06-04 19:48:42 +0600

    conference room booking rest of time slot showing

--- src/employee/forms/employee_project.py
+++ src/employee/forms/employee_project.py
@@ -40,6 +40,7 @@ class BookConferenceRoomForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        current_time = timezone.now().time()
         booked_times = BookConferenceRoom.objects.filter(
             created_at__date=timezone.now().date()
         ).values_list(
@@ -48,8 +49,7 @@ class BookConferenceRoomForm(forms.ModelForm):
         )
         # Filter out booked times from TIME_CHOICES
         available_choices = [
-            choice
-            for choice in BookConferenceRoom.TIME_CHOICES
-            if choice[0] not in booked_times
+            choice for choice in BookConferenceRoom.TIME_CHOICES
+            if choice[0] > current_time and choice[0] not in booked_times
         ]
         self.fields["start_time"].choices = available_choices
