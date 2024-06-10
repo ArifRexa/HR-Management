a6899266e5cb23e8cd875116d27b08b256a56de0
shaheen2013 <coredeveloper.2013@gmail.com>
2024-06-10 13:47:25 +0600

    solved increament nofication issue

--- src/employee/management/commands/increment_notifi.py
+++ src/employee/management/commands/increment_notifi.py
@@ -9,7 +9,7 @@ from employee.tasks import increment_notification
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        data_to_check = datetime.datetime.now() - datetime.timedelta(days=100)
+        data_to_check = datetime.datetime.now() - datetime.timedelta(days=160)
         
         employees = Employee.objects.filter(
             salaryhistory__active_from__exact=data_to_check,
