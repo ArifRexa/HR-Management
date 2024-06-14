3e2593ce906d65d8e3e0e510ba3e5d79f82fd969
mdborhanst <mdborhan.st@gmail.com>
2024-06-14 18:37:17 +0600

    enroll api view create

--- src/academy/urls.py
+++ src/academy/urls.py
@@ -1,8 +1,9 @@
-from academy.views import MarketingSliderAPIListView, TrainingListAPIView, TrainingRetrieveAPIView
+from academy.views import MarketingSliderAPIListView, StudentCreateAPIView, TrainingListAPIView, TrainingRetrieveAPIView
 from django.urls import path
 
 urlpatterns = [
     path("marketing/sliders/", MarketingSliderAPIListView.as_view(), name="marketing_slider"),
     path("training/<int:pk>/", TrainingRetrieveAPIView.as_view(), name="training"),
     path("trainings/", TrainingListAPIView.as_view(), name="trainings"),
+    path("student/", StudentCreateAPIView.as_view(), name="student"),
 ]
