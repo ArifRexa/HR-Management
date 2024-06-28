9d37e0f621cc04a083f13463c5a711db544a1b54
mdborhanst <mdborhan.st@gmail.com>
2024-06-28 20:33:14 +0600

    update services details serializer

--- src/website/serializers.py
+++ src/website/serializers.py
@@ -4,7 +4,10 @@ from employee.models import (
     Employee,
     EmployeeSocial,
     EmployeeContent,
-    EmployeeNOC, Skill, EmployeeSkill,EmployeeExpertTech
+    EmployeeNOC,
+    Skill,
+    EmployeeSkill,
+    EmployeeExpertTech,
 )
 from project_management.models import (
     Project,
@@ -23,14 +26,8 @@ from project_management.models import (
     ProjectResults,
     OurTechnology,
     ProjectPlatform,
-    ProjectIndustry, 
+    ProjectIndustry,
     ProjectService,
-    
-    
-    
-    
-    
-
 )
 from settings.models import Designation
 from website.models import (
@@ -51,24 +48,26 @@ from website.models import (
     EmployeePerspective,
     Industry,
     Lead,
-    ServiceContent
+    ServiceContent,
 )
 
 
 class ProjectPlatformSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectPlatform
-        fields = ('title',)
+        fields = ("title",)
+
 
 class ProjectIndustrySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectIndustry
-        fields = ('title',)
+        fields = ("title",)
+
 
 class ProjectServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectService
-        fields = ('title',)
+        fields = ("title",)
 
 
 class TechnologySerializer(serializers.ModelSerializer):
@@ -79,14 +78,28 @@ class TechnologySerializer(serializers.ModelSerializer):
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ("name", "email","designation", "address", "country", "logo","client_feedback")
+        fields = (
+            "name",
+            "email",
+            "designation",
+            "address",
+            "country",
+            "logo",
+            "client_feedback",
+        )
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["projects"] = ProjectSerializer(
+            instance=instance.project_set.filter(active=True), many=True
+        ).data
+        return data
+
 
 class OurClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ("name", "designation", "logo","client_feedback")
-
-
+        fields = ("name", "designation", "logo", "client_feedback")
 
 
 class ServiceTechnologySerializer(serializers.ModelSerializer):
@@ -101,39 +114,69 @@ class ServiceProcessSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceProcess
         fields = "__all__"
-        
+
 
 class IndustrySerializer(serializers.ModelSerializer):
     technology = TechnologySerializer(many=True)
+
     class Meta:
         model = Industry
         fields = "__all__"
-        
 
 
 class ServiceSerializer(serializers.ModelSerializer):
-    technologies = ServiceTechnologySerializer(many=True, source="servicetechnology_set")
-    industry = IndustrySerializer(many=True) 
+    technologies = ServiceTechnologySerializer(
+        many=True, source="servicetechnology_set"
+    )
+    industry = IndustrySerializer(many=True)
+
     class Meta:
         model = Service
-        fields = ("title", "slug","short_description","feature","technologies","industry","feature_image")
+        fields = (
+            "title",
+            "sub_title",
+            "slug",
+            "short_description",
+            "feature",
+            "technologies",
+            "industry",
+            "feature_image",
+        )
 
 
 class ServiceContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceContent
-        fields = ("title", "content","image",)
+        fields = (
+            "title",
+            "content",
+            "image",
+        )
 
 
 class ServiceDetailsSerializer(serializers.ModelSerializer):
     clients = ClientSerializer(many=True)
-    technologies = ServiceTechnologySerializer(many=True, source="servicetechnology_set")
+    technologies = ServiceTechnologySerializer(
+        many=True, source="servicetechnology_set"
+    )
     service_process = ServiceProcessSerializer(many=True)
     service_contents = ServiceContentSerializer(many=True, source="servicecontent_set")
+
     class Meta:
         model = Service
-        fields = ("slug","title","short_description","banner_image","feature_image","feature","service_process","technologies","service_contents","clients")
-
+        fields = (
+            "slug",
+            "title",
+            "sub_title",
+            "short_description",
+            "banner_image",
+            "feature_image",
+            "feature",
+            "service_process",
+            "technologies",
+            "service_contents",
+            "clients",
+        )
 
 
 class ProjectTechnologySerializer(serializers.ModelSerializer):
@@ -149,6 +192,7 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ("name",)
 
+
 class AvailableTagSerializer(serializers.ModelSerializer):
     tags_count = serializers.SerializerMethodField()
 
@@ -157,7 +201,8 @@ class AvailableTagSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "tags_count")
 
     def get_tags_count(self, obj):
-        return Project.objects.filter(tags=obj,show_in_website=True).count()
+        return Project.objects.filter(tags=obj, show_in_website=True).count()
+
 
 class ClientFeedbackSerializer(serializers.ModelSerializer):
     class Meta:
@@ -189,8 +234,7 @@ class ProjectClientFeedbackSerializer(serializers.ModelSerializer):
 class ProjectKeyFeatureSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectKeyFeature
-        fields = ("title","description","img","img2")
-
+        fields = ("title", "description", "img", "img2")
 
 
 class ProjectResultsSerializer(serializers.ModelSerializer):
@@ -203,9 +247,12 @@ class ProjectResultsSerializer(serializers.ModelSerializer):
             "increased_order_rate",
         )
 
+
 class ProjectSerializer(serializers.ModelSerializer):
-    technologies = ProjectTechnologySerializer(many=True, source="projecttechnology_set")
-    project_results = ProjectResultsSerializer() 
+    technologies = ProjectTechnologySerializer(
+        many=True, source="projecttechnology_set"
+    )
+    project_results = ProjectResultsSerializer()
     industries = serializers.SerializerMethodField()
 
     class Meta:
@@ -221,31 +268,36 @@ class ProjectSerializer(serializers.ModelSerializer):
         )
 
     def get_industries(self, obj):
-
         return [industry.title for industry in obj.industries.all()]
+
 
 class ProjectContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectContent
-        fields = ("title", "content","image","image2")
-
+        fields = ("title", "content", "image", "image2")
 
 
 class ProjectDetailsSerializer(serializers.ModelSerializer):
-    technologies = ProjectTechnologySerializer(many=True, source="projecttechnology_set")
+    technologies = ProjectTechnologySerializer(
+        many=True, source="projecttechnology_set"
+    )
     available_tags = TagSerializer(read_only=True, many=True, source="tags")
     client = ClientSerializer()
-    client_feedback = ProjectClientFeedbackSerializer(many=True, source="clientfeedback_set") 
+    client_feedback = ProjectClientFeedbackSerializer(
+        many=True, source="clientfeedback_set"
+    )
     project_design = ProjectScreenshotSerializer(
         source="projectscreenshot_set", many=True, read_only=True
     )
     project_contents = ProjectContentSerializer(many=True, source="projectcontent_set")
-    project_key_feature = ProjectKeyFeatureSerializer(many=True,source="projectkeyfeature_set")
-    project_results = ProjectResultsSerializer() 
+    project_key_feature = ProjectKeyFeatureSerializer(
+        many=True, source="projectkeyfeature_set"
+    )
+    project_results = ProjectResultsSerializer()
     platforms = ProjectPlatformSerializer(many=True)
     industries = ProjectIndustrySerializer(many=True)
     services = ProjectServiceSerializer(many=True)
-    
+
     class Meta:
         model = Project
         fields = (
@@ -268,17 +320,25 @@ class ProjectDetailsSerializer(serializers.ModelSerializer):
             "client_feedback",
             "project_design",
         )
-        
 
 
 class ProjectHighlightedSerializer(serializers.ModelSerializer):
-    
-    project_results = ProjectResultsSerializer() 
-    technologies = ProjectTechnologySerializer(many=True, source="projecttechnology_set")
-    
+    project_results = ProjectResultsSerializer()
+    technologies = ProjectTechnologySerializer(
+        many=True, source="projecttechnology_set"
+    )
+
     class Meta:
         model = Project
-        fields = ("slug","title","description","project_results","thumbnail","technologies")
+        fields = (
+            "slug",
+            "title",
+            "description",
+            "project_results",
+            "thumbnail",
+            "technologies",
+        )
+
 
 class EmployeeSocialSerializer(serializers.ModelSerializer):
     class Meta:
@@ -290,6 +350,7 @@ class EmployeeContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmployeeContent
         fields = ("title", "content")
+
 
 class EmployeeSerializer(serializers.ModelSerializer):
     designation = serializers.StringRelatedField(many=False)
@@ -331,17 +392,18 @@ class EmployeeDetailsSerializer(serializers.ModelSerializer):
         )
 
 
-
-
-
 class DesignationSetSerializer(serializers.ModelSerializer):
     employee_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Designation
-        fields = ['id','title', 'employee_count']
+        fields = ["id", "title", "employee_count"]
 
     def get_employee_count(self, obj):
-        return Employee.objects.filter(designation=obj,active=True, show_in_web=True).count()
+        return Employee.objects.filter(
+            designation=obj, active=True, show_in_web=True
+        ).count()
+
 
 # class EmployeeSkillSerializer(serializers.ModelSerializer):
 #     class Meta:
@@ -354,10 +416,12 @@ class SkillSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Skill
-        fields = ['id', 'title', 'total_employees']
+        fields = ["id", "title", "total_employees"]
 
     def get_total_employees(self, obj):
-        return Employee.objects.filter(employeeskill__skill=obj,active=True, show_in_web=True).count()
+        return Employee.objects.filter(
+            employeeskill__skill=obj, active=True, show_in_web=True
+        ).count()
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
@@ -366,7 +430,14 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Employee
-        fields = ['id', 'full_name', 'designation', 'image','operation', 'employeeskill']
+        fields = [
+            "id",
+            "full_name",
+            "designation",
+            "image",
+            "operation",
+            "employeeskill",
+        ]
 
     def get_designation(self, obj):
         if obj.designation:
@@ -376,7 +447,11 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
     def get_employeeskill(self, obj):
         skills = EmployeeSkill.objects.filter(employee=obj)
-        return [{'skill': skill.skill.title, 'percentage': skill.percentage} for skill in skills]
+        return [
+            {"skill": skill.skill.title, "percentage": skill.percentage}
+            for skill in skills
+        ]
+
 
 class CategoryListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -463,6 +538,7 @@ class BlogListSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField("get_category")
     author = AuthorSerializer(source="created_by.employee")
     blog_contexts = BlogContextSerializer(many=True)
+
     class Meta:
         model = Blog
         fields = (
@@ -476,7 +552,7 @@ class BlogListSerializer(serializers.ModelSerializer):
             "total_view",
             "created_at",
             "author",
-            'blog_contexts'
+            "blog_contexts",
         )
 
     def get_category(self, instance):
@@ -586,14 +662,13 @@ class OurTechnologySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OurTechnology
-        fields = ('title', 'technologies') 
+        fields = ("title", "technologies")
 
 
 class FAQSerializer(serializers.ModelSerializer):
     class Meta:
         model = FAQ
-        fields = ("question","answer")
-   
+        fields = ("question", "answer")
 
 
 class OurClientsFeedbackSerializer(serializers.ModelSerializer):
@@ -604,7 +679,7 @@ class OurClientsFeedbackSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Project
-        fields = ('client_name', 'client_designation', 'client_logo', 'feedback')
+        fields = ("client_name", "client_designation", "client_logo", "feedback")
 
     def get_client_name(self, obj):
         return obj.client.name if obj.client else None
@@ -614,67 +689,76 @@ class OurClientsFeedbackSerializer(serializers.ModelSerializer):
 
     def get_client_logo(self, obj):
         if obj.client and obj.client.logo:
-            return self.context['request'].build_absolute_uri(obj.client.logo.url)
+            return self.context["request"].build_absolute_uri(obj.client.logo.url)
         return None
 
     def get_feedback(self, obj):
         clientfeedback = ClientFeedback.objects.filter(project=obj)
         serializers = ClientFeedbackSerializer(instance=clientfeedback, many=True)
         return serializers.data
-    
+
 
 class OurAchievementSerializer(serializers.ModelSerializer):
     class Meta:
         model = OurAchievement
-        fields = ("title","number")
+        fields = ("title", "number")
 
 
 class OurGrowthSerializer(serializers.ModelSerializer):
     class Meta:
         model = OurGrowth
-        fields = ("title","number")
+        fields = ("title", "number")
 
-    
 
 class OurJourneySerializer(serializers.ModelSerializer):
     class Meta:
         model = OurJourney
-        fields = ("year","title","description","img")
+        fields = ("year", "title", "description", "img")
 
 
 class EmployeePerspectiveSerializer(serializers.ModelSerializer):
-    employee_name = serializers.CharField(source='employee.full_name',read_only =True)
-    employee_designation = serializers.CharField(source='employee.designation',read_only=True)
-    employee_image= serializers.ImageField(source='employee.image',read_only=True)
+    employee_name = serializers.CharField(source="employee.full_name", read_only=True)
+    employee_designation = serializers.CharField(
+        source="employee.designation", read_only=True
+    )
+    employee_image = serializers.ImageField(source="employee.image", read_only=True)
+
     class Meta:
         model = EmployeePerspective
-        fields = ("title","description","employee_name","employee_designation","employee_image",)
+        fields = (
+            "title",
+            "description",
+            "employee_name",
+            "employee_designation",
+            "employee_image",
+        )
 
 
 class LeadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lead
-        fields = ['name', 'email', 'message']
-        
+        fields = ["name", "email", "message"]
+
 
 class ClientLogoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ['logo']
-        
+        fields = ["logo"]
+
+
 class GallerySerializer(serializers.ModelSerializer):
     class Meta:
         model = Gallery
-        fields = ['image']
-        
+        fields = ["image"]
+
 
 class AwardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Award
-        fields = ['image']
-        
+        fields = ["image"]
+
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ['name', 'designation', 'image', 'client_feedback']
+        fields = ["name", "designation", "image", "client_feedback"]
