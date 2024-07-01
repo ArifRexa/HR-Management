ba4bbecabb5c04350f004f14c8726ce6db621cec
mdborhanst <mdborhan.st@gmail.com>
2024-07-01 19:35:31 +0600

    create new income invoice html and query

--- src/account/admin/income.py
+++ src/account/admin/income.py
@@ -17,31 +17,35 @@ from config.utils.pdf import PDF
 
 @admin.register(Income)
 class IncomeAdmin(admin.ModelAdmin):
-    list_display = ('project', 'date', 'hours', 'loss_hours',
-                    'hour_rate', 'convert_rate', 'payment_details', 'status_col')
-    date_hierarchy = 'date'
-    exclude = ['is_send_clients']
-    readonly_fields = ('payment',)
-    list_filter = ('status', 'project', 'hour_rate', 'date')
-    actions = ['approve_selected', 'pending_selected', 'print_income_invoices']
+    list_display = (
+        "project",
+        "date",
+        "hours",
+        "loss_hours",
+        "hour_rate",
+        "convert_rate",
+        "payment_details",
+        "status_col",
+    )
+    date_hierarchy = "date"
+    exclude = ["is_send_clients"]
+    readonly_fields = ("payment",)
+    list_filter = ("status", "project", "hour_rate", "date")
+    actions = ["approve_selected", "pending_selected", "print_income_invoices"]
     # list_editable = ('status',)
-    formfield_overrides = {
-        models.TextField: {'widget': Textarea(attrs={'rows': 2})}
-    }
-    autocomplete_fields = ['project']
+    formfield_overrides = {models.TextField: {"widget": Textarea(attrs={"rows": 2})}}
+    autocomplete_fields = ["project"]
 
-    change_list_template = 'admin/income/list.html'
+    change_list_template = "admin/income/list.html"
 
     list_per_page = 20
 
-    @admin.action(description='Status')
+    @admin.action(description="Status")
     def status_col(self, obj):
-        color = 'red'
-        if obj.status == 'approved':
-            color = 'green'
-        return format_html(
-            f'<b style="color: {color}">{obj.get_status_display()}</b>'
-        )
+        color = "red"
+        if obj.status == "approved":
+            color = "green"
+        return format_html(f'<b style="color: {color}">{obj.get_status_display()}</b>')
 
     @admin.display()
     def payment_details(self, obj):
@@ -51,42 +55,66 @@ class IncomeAdmin(admin.ModelAdmin):
         )
 
     def get_total_hour(self, request):
-        filters = dict([(key, request.GET.get(key)) for key in dict(request.GET) if key not in ['p', 'o']])
+        filters = dict(
+            [
+                (key, request.GET.get(key))
+                for key in dict(request.GET)
+                if key not in ["p", "o"]
+            ]
+        )
         if not request.user.is_superuser:
-            filters['created_by__id__exact'] = request.user.employee.id
-        dataset = Income.objects.filter(*[Q(**{key: value}) for key, value in filters.items() if value])
+            filters["created_by__id__exact"] = request.user.employee.id
+        dataset = Income.objects.filter(
+            *[Q(**{key: value}) for key, value in filters.items() if value]
+        )
         return {
-            'total_pending': dataset.filter(status='pending').aggregate(total=Coalesce(Sum('payment'), 0.0))['total'],
-            'total_pending_usd': dataset.filter(status='pending').aggregate(
-                total=Coalesce(Sum(F('payment') / F('convert_rate')), 0.0))['total'],
-            'total_paid': dataset.filter(status='approved').aggregate(total=Coalesce(Sum('payment'), 0.0))['total'],
-            'total_paid_usd': dataset.filter(status='approved').aggregate(
-                total=Coalesce(Sum(F('payment') / F('convert_rate')), 0.0))['total'],
-            'pending_hour': dataset.filter(status='pending').aggregate(total=Coalesce(Sum('hours'), 0.0))['total'],
-            'approved_hour': dataset.filter(status='approved').aggregate(total=Coalesce(Sum('hours'), 0.0))['total'],
-            'total_loss_hours': dataset.aggregate(total=Coalesce(Sum('loss_hours'), 0.0))['total']
+            "total_pending": dataset.filter(status="pending").aggregate(
+                total=Coalesce(Sum("payment"), 0.0)
+            )["total"],
+            "total_pending_usd": dataset.filter(status="pending").aggregate(
+                total=Coalesce(Sum(F("payment") / F("convert_rate")), 0.0)
+            )["total"],
+            "total_paid": dataset.filter(status="approved").aggregate(
+                total=Coalesce(Sum("payment"), 0.0)
+            )["total"],
+            "total_paid_usd": dataset.filter(status="approved").aggregate(
+                total=Coalesce(Sum(F("payment") / F("convert_rate")), 0.0)
+            )["total"],
+            "pending_hour": dataset.filter(status="pending").aggregate(
+                total=Coalesce(Sum("hours"), 0.0)
+            )["total"],
+            "approved_hour": dataset.filter(status="approved").aggregate(
+                total=Coalesce(Sum("hours"), 0.0)
+            )["total"],
+            "total_loss_hours": dataset.aggregate(
+                total=Coalesce(Sum("loss_hours"), 0.0)
+            )["total"],
         }
 
     def changelist_view(self, request, extra_context=None):
         my_context = {
-            'result': self.get_total_hour(request),
+            "result": self.get_total_hour(request),
         }
         return super().changelist_view(request, extra_context=my_context)
 
     @admin.action()
     def approve_selected(self, request, queryset):
-        queryset.update(status='approved')
+        queryset.update(status="approved")
         # self.message_user(request, f'Status has been updated to approved for {len(queryset)} items', messages.SUCCESS)
 
     @admin.action()
     def pending_selected(self, request, queryset):
-        queryset.update(status='pending')
+        queryset.update(status="pending")
         # self.message_user(request, f'Status has been updated to pending for {len(queryset)} items', messages.SUCCESS)
 
     def get_urls(self):
         urls = super().get_urls()
         my_urls = [
-            path('balance/', self.admin_site.admin_view(self.balance_view), name='account_balance'),
+            path(
+                "balance/",
+                self.admin_site.admin_view(self.balance_view),
+                name="account_balance",
+            ),
         ]
         return my_urls + urls
 
@@ -96,20 +124,30 @@ class IncomeAdmin(admin.ModelAdmin):
             context = dict(
                 self.admin_site.each_context(request),
                 data=balance,
-                title=f'Profit / Loss'
+                title=f"Profit / Loss",
             )
             return TemplateResponse(request, "admin/balance/balance.html", context)
         raise PermissionDenied
 
     @admin.action()
     def print_income_invoices(self, request, queryset):
+        queryset = queryset.order_by("date")
+        income_list = queryset.values_list("id", flat=True)
+        id_str = "_".join(list(map(str, income_list)))
+        project_name = queryset.first().project.title
         pdf = PDF()
-        pdf.file_name = f'Income Invoice'
-        pdf.template_path = "compliance/income_invoice.html"
+        pdf.file_name = f"Income Invoice-{project_name}-{id_str}"
+        pdf.template_path = "compliance/new_income_invoice.html"
         protocal = "https" if request.is_secure() else "http"
+        invoice_total = (
+            queryset.values("date", "hour_rate")
+            .annotate(project_total=F("hours") * F("hour_rate"))
+            .aggregate(total = Sum("project_total"))
+        )
         pdf.context = {
-            'invoices': queryset,
-            'seal': f"{STATIC_ROOT}/stationary/sign_md.png",
-            "host": f"{protocal}://{request.get_host()}"
+            "invoices": queryset,
+            "seal": f"{STATIC_ROOT}/stationary/sign_md.png",
+            "host": f"{protocal}://{request.get_host()}",
+            "invoice_total": invoice_total.get("total")
         }
         return pdf.render_to_pdf(download=True)
