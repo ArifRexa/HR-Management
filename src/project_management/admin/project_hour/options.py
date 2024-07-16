00059ffea3f03c053202c028acb0c01e120becb0
mdborhanst <mdborhan.st@gmail.com>
2024-07-16 19:23:34 +0600

    add permission for filter by client

--- src/project_management/admin/project_hour/options.py
+++ src/project_management/admin/project_hour/options.py
@@ -163,6 +163,7 @@ class ProjectHourOptions(admin.ModelAdmin):
         filters = [ProjectTypeFilter, ProjectFilter, "project__client__payment_method", "project__client"]
         if not request.user.has_perm("project_management.view_client"):
             filters.remove("project__client__payment_method")
+            filters.remove("project__client")
         return filters
 
     def get_list_display(self, request):
