db9b61e66c5f9688615ee87cf6edf0a83e64b038
MD. Iubullah Biplobe <biplobe6@gmail.com>
2024-07-16 17:45:28 +0600

    Updated: Employee Under TPM (last week hours)

--- src/employee/admin/employee/employee.py
+++ src/employee/admin/employee/employee.py
@@ -457,58 +457,16 @@ class EmployeeUnderTPMAdmin(admin.ModelAdmin):
 
         tpm_builder = TPMsBuilder()
 
-        # Group employee by tpm
-        tpm_data = {}
-        temp_tpm_data = {}
         for employee in tpm_project_data:
             tpm_builder.get_or_create(employee)
 
-            tpm_id = employee.tpm.pk
-            employee_id = employee.employee.pk
-            project_id = employee.project_id
-            client_id = None
-            if employee.project.client:
-                client_id = employee.project.client.pk
-
-            if tpm_id not in tpm_data:
-                tpm_data[tpm_id] = {
-                    'grouper': employee.tpm,
-                    'list': [],
-
-                    'employees': [],
-                    'projects': [],
-                    'clients': [],
-                }
-                temp_tpm_data[tpm_id] = {
-                    '__employee_ids': [],
-                    '__project_ids': [],
-                    '__client_ids': [],
-                }
-
-            grouper = tpm_data[tpm_id]
-            tmp_grouper = temp_tpm_data[tpm_id]
-
-            if employee_id not in tmp_grouper['__employee_ids']:
-                tmp_grouper['__employee_ids'].append(employee_id)
-                grouper['list'].append(employee)
-                grouper['employees'].append(employee.employee)
-
-            if project_id not in tmp_grouper['__project_ids']:
-                tmp_grouper['__project_ids'].append(project_id)
-                grouper['projects'].append(employee.project)
-
-            if client_id and (client_id not in tmp_grouper['__client_ids']):
-                tmp_grouper['__client_ids'].append(client_id)
-                grouper['clients'].append(employee.project.client)
-
-        print(tpm_builder.tpm_list)
+        tpm_builder.update_hours_count()
 
         my_context = {
             "tpm_project_data": tpm_project_data,
-            "tpm_data": list(tpm_data.values()),
+            "tpm_data": tpm_builder.tpm_list,
         }
 
-        # print(tpm_project_data.prject.all())
         return super().changelist_view(request, extra_context=my_context)
 
     def get_urls(self):
--- src/employee/helper/tpm.py
+++ src/employee/helper/tpm.py
@@ -11,6 +11,7 @@ from employee.models.employee import (
     EmployeeUnderTPM,
 )
 from project_management.models import Project
+from project_management.models import Client
 from project_management.models import EmployeeProjectHour
 
 
@@ -21,19 +22,53 @@ WORKING_DAYS_IN_A_WEEK = 5
 
 
 
+def _get_date_range_by_week(week=0, days=7):
+    """
+    week: int
+        1 (next week)
+        0 (current week)
+        -1 (last week)
+        -2 (2nd last week)
+    """
+    now = datetime.now()
+    start_day_of_year = datetime(
+        year=now.year,
+        month=1,
+        day=1,
+    )
+    week_day_of_year_start = start_day_of_year.weekday()
+    target_week = (now + timedelta(weeks=week)).isocalendar()[1]
+
+    start_day = (
+        start_day_of_year
+        + timedelta(
+            weeks=target_week - 1,
+            days=-week_day_of_year_start
+        )
+    ).date()
+    end_date = start_day + timedelta(
+        days=days - 1
+    )
+    return (start_day, end_date)
+
+
+
 @dataclass
 class TPMObj:
     tpm: Employee
-    # last_week_hours: t.List[float] = field(default_factory=lambda: [0.0]*4, init=False)
+    last_week_date_range: t.List[t.Tuple[t.Optional[datetime]]] = field(default_factory=lambda: [(None, None)]*4, init=False)
+    last_week_hours: t.List[float] = field(default_factory=lambda: [0.0]*4, init=False)
 
     employees: t.List[Employee] = field(default_factory=list, init=False)
     projects: t.List[Project] = field(default_factory=list, init=False)
+    clients: t.List[Client] = field(default_factory=list, init=False)
 
     _employee_ids: t.List[int] = field(default_factory=list, init=False)
     _project_ids: t.List[int] = field(default_factory=list, init=False)
 
     __employee_hash: t.Dict[int, Employee] = field(default_factory=dict, init=False, repr=False)
     __project_hash: t.Dict[int, Project] = field(default_factory=dict, init=False, repr=False)
+    __client_hash: t.Dict[int, Client] = field(default_factory=dict, init=False, repr=False)
     __cached_employee_hours_qs: t.Optional[Manager[EmployeeProjectHour]] = field(default=None, init=False, repr=False)
 
     def add_employee(self, data: Employee):
@@ -56,9 +91,19 @@ class TPMObj:
         self.__project_hash[project_id] = data
         self.projects.append(data)
 
+    def add_client(self, data: t.Optional[Client]):
+        if not data:
+            return
+        client_id = data.pk
+        if client_id in self.__client_hash:
+            return
+
+        self.__client_hash[client_id] = data
+        self.clients.append(data)
+
     def _get_employee_hours(self) -> Manager[EmployeeProjectHour]:
         if self.__cached_employee_hours_qs is None:
-            start_date, end_date = self._get_date_range_by_week(-1)
+            start_date, end_date = _get_date_range_by_week(-1)
             start_date = end_date - timedelta(days=7*4) # last 4 weeks
             self.__cached_employee_hours_qs = EmployeeProjectHour.objects.filter(
                 employee__in=self._employee_ids,
@@ -70,82 +115,38 @@ class TPMObj:
             )
         return self.__cached_employee_hours_qs
 
-    def _get_date_range_by_week(self, week=0, days=7):
-        """
-        week: int
-            1 (next week)
-            0 (current week)
-            -1 (last week)
-            -2 (2nd last week)
-        """
-        now = datetime.now()
-        start_day_of_year = datetime(
-            year=now.year,
-            month=1,
-            day=1,
-        )
-        week_day_of_year_start = start_day_of_year.weekday()
-        target_week = (now + timedelta(weeks=week)).isocalendar()[1]
-
-        start_day = (
-            start_day_of_year
-            + timedelta(
-                weeks=target_week - 1,
-                days=-week_day_of_year_start
-            )
-        ).date()
-        end_date = start_day + timedelta(
-            days=days - 1
-        )
-        return (start_day, end_date)
-
-    def _get_hours_count(self, start_date: datetime, end_date: datetime) -> float:
-        hours = 0.0
-        employee_hours = self._get_employee_hours()
-        for entry in employee_hours:
-            entry_date = entry.project_hour.date
-            if entry_date <= end_date and entry_date > start_date:
-                hours += entry.hours
-        return hours
-
-    @property
-    def last_week_hours(self) -> float:
-        start_date, end_date = self._get_date_range_by_week(week=-1, days=WORKING_DAYS_IN_A_WEEK)
-        hours = self._get_hours_count(
-            start_date=start_date,
-            end_date=end_date
-        )
-        return hours
-
-    @property
-    def last_week_hours_2nd(self) -> float:
-        start_date, end_date = self._get_date_range_by_week(week=-2, days=WORKING_DAYS_IN_A_WEEK)
-        hours = self._get_hours_count(
-            start_date=start_date,
-            end_date=end_date
-        )
-        return hours
-
-    @property
-    def last_week_hours_3rd(self) -> float:
-        start_date, end_date = self._get_date_range_by_week(week=-3, days=WORKING_DAYS_IN_A_WEEK)
-        hours = self._get_hours_count(
-            start_date=start_date,
-            end_date=end_date
-        )
-        return hours
-
-    @property
-    def last_week_hours_4th(self) -> float:
-        start_date, end_date = self._get_date_range_by_week(week=-4, days=WORKING_DAYS_IN_A_WEEK)
-        hours = self._get_hours_count(
-            start_date=start_date,
-            end_date=end_date
-        )
+    def _update_hours_count(
+            self,
+            data: EmployeeProjectHour,
+            start_date: datetime,
+            end_date: datetime,
+            week_index=0
+        ) -> float:
+        hours = self.last_week_hours[week_index]
+        data_date = data.project_hour.date
+        data_hours = data.hours or 0.0
+        if data_date <= end_date and data_date > start_date:
+            hours += data_hours
+        self.last_week_hours[week_index] = hours
         return hours
 
     def update_hours_count(self):
-        pass
+        self.last_week_hours = [0.0] * 4
+        self.last_week_date_range = week_ranges = [
+            _get_date_range_by_week(week=-1, days=WORKING_DAYS_IN_A_WEEK),
+            _get_date_range_by_week(week=-2, days=WORKING_DAYS_IN_A_WEEK),
+            _get_date_range_by_week(week=-3, days=WORKING_DAYS_IN_A_WEEK),
+            _get_date_range_by_week(week=-4, days=WORKING_DAYS_IN_A_WEEK),
+        ]
+        employee_hours = self._get_employee_hours()
+        for entry in employee_hours:
+            for week_index, (start_date, end_date) in enumerate(week_ranges):
+                self._update_hours_count(
+                    data=entry,
+                    start_date=start_date,
+                    end_date=end_date,
+                    week_index=week_index
+                )
 
 
 
@@ -161,12 +162,12 @@ class TPMsBuilder:
             tpm = TPMObj(
                 tpm=data.tpm
             )
-            tpm.add_employee(data=data.employee)
-            tpm.add_project(data=data.project)
-
             self.__tpm_hash[tpm_id] = tpm
             self.tpm_list.append(tpm)
         tpm = self.__tpm_hash[tpm_id]
+        tpm.add_employee(data=data.employee)
+        tpm.add_project(data=data.project)
+        tpm.add_client(data=data.project.client)
         return tpm
 
     def update_hours_count(self):
