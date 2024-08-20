3e75eb0fba251d6f5cba1b2efaf6a16d98a04395
mdborhanst <mdborhan.st@gmail.com>
2024-08-20 19:50:35 +0600

    add javascript in blog admin

--- src/website/admin/admin.py
+++ src/website/admin/admin.py
@@ -252,21 +252,21 @@ class BlogAdmin(admin.ModelAdmin):
         else:
             return querySet.filter(created_by=user)
 
-    def get_form(
-        self,
-        request: Any,
-        obj: Union[Any, None] = ...,
-        change: bool = ...,
-        **kwargs: Any,
-    ) -> Any:
-        try:
-            form = super().get_form(request, obj, change, **kwargs)
-            form.base_fields["active"].disabled = not request.user.has_perm(
-                "website.can_approve"
-            )
-        except Exception:
-            form = super().get_form(request, obj, **kwargs)
-        return form
+    # def get_form(
+    #     self,
+    #     request: Any,
+    #     obj: Union[Any, None] = ...,
+    #     change: bool = ...,
+    #     **kwargs: Any,
+    # ) -> Any:
+    #     try:
+    #         form = super().get_form(request, obj, change, **kwargs)
+    #         form.base_fields["active"].disabled = not request.user.has_perm(
+    #             "website.can_approve"
+    #         )
+    #     except Exception:
+    #         form = super().get_form(request, obj, **kwargs)
+    #     return form
 
     def has_change_permission(self, request, obj=None):
         permitted = super().has_change_permission(request, obj=obj)
@@ -287,11 +287,11 @@ class BlogAdmin(admin.ModelAdmin):
             return not obj.active and obj.created_by == user
         return permitted
 
-    def save_model(self, request: Any, obj: Any, form: Any, change: Any) -> None:
-        form.base_fields["active"].disabled = not request.user.has_perm(
-            "website.can_approve"
-        )
-        return super().save_model(request, obj, form, change)
+    # def save_model(self, request: Any, obj: Any, form: Any, change: Any) -> None:
+    #     form.base_fields["active"].disabled = not request.user.has_perm(
+    #         "website.can_approve"
+    #     )
+    #     return super().save_model(request, obj, form, change)
 
 
 @admin.register(BlogComment)
--- src/website/static/js/blog_post_field_escape.js
+++ src/website/static/js/blog_post_field_escape.js
@@ -5,11 +5,15 @@ window.onload = function() {
     // console.log(blog_content_el.innerText);
 
     const blog_context_group = document.getElementById("blog_contexts-group")
-    const blog_context_field_container = blog_context_group.getElementsByClassName("inline-related")[0]
-    const blog_context_description = blog_context_field_container.getElementsByClassName("form-row field-description")[0]
-    const description = blog_context_description.getElementsByClassName("readonly")[0]
-    description.innerHTML = description.innerText
-    console.log(description);
+    const blog_context_field_container = blog_context_group.getElementsByClassName("inline-related")
+    for (let i = 0; i < blog_context_field_container.length; i++) {
+
+        const blog_context_description = blog_context_field_container[i].getElementsByClassName("form-row field-description")[0]
+        const description = blog_context_description.getElementsByClassName("readonly")[0]
+        description.innerHTML = description.innerText
+        console.log(description);
+
+    }
     
     
 }
