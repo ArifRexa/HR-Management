4b141ac90e631b6c939301c21c5d22c01ada7db0
mdborhanst <mdborhan.st@gmail.com>
2024-08-21 18:49:37 +0600

    update feedback send task and template

--- src/website/tasks.py
+++ src/website/tasks.py
@@ -2,13 +2,18 @@ from django.core.mail import EmailMultiAlternatives
 
 from django.template.loader import get_template
 
+from website.models import BlogModeratorFeedback
 
-def send_blog_moderator_feedback_email(content, employee):
+
+def send_blog_moderator_feedback_email(content, blog: BlogModeratorFeedback, url):
+    employee = blog.created_by.employee
     html_template = get_template("blog/mail/moderator_feedback.html")
-    html_content = html_template.render({"content": content, "employee": employee})
+    html_content = html_template.render(
+        {"content": content, "employee": employee, "blog": blog, "url": url}
+    )
 
     email = EmailMultiAlternatives(subject="Mediusware Blog - Feedback From Moderator")
     email.attach_alternative(html_content, "text/html")
     email.to = [employee.email]
-    email.from_email = "Mediusware Ltd. <no-reply@mediusware.com>"
+    email.from_email = '"Mediusware-Admin" <admin@mediusware.com>'
     email.send()
