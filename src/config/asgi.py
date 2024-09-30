f1b76505fa0eedcf1fdca484e18dc4d45923caf6
mdborhanst <mdborhan.st@gmail.com>
2024-09-30 14:17:18 +0600

    update asgi file

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -8,13 +8,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import dotenv
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routers import websocket_urlpatterns
-# Import your websocket routes if you have any
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# Import your websocket routes if you have any
+from chat.routers import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
