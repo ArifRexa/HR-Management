461c989a956707cdd97be3762f9430e0c3b83b49
dev.afat <shaonmohammad13@gmail.com>
2024-10-16 12:31:38 +0600

    solved logo issue and auto reload reception page after each 5 minute

--- src/reception/admin.py
+++ src/reception/admin.py
@@ -4,13 +4,25 @@ from django.utils.html import format_html
 
 @admin.register(Reception)
 class ReceptionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'agenda','get_comment','created_at','status')
-    list_filter = ('agenda','status','created_at')
+    list_display = ('name', 'agenda','get_comment','created_at','get_status')
+    list_filter = ('status','agenda')
     date_hierarchy = ('created_at')
+
+    class Media:
+        js = ("js/reception.js",)
     def get_comment(self, obj):
         if obj.comment:
             truncated_comment = ' '.join(obj.comment.split()[:4]) + '...'
             return format_html('<span title="{}">{}</span>', obj.comment, truncated_comment)
         return '-'
-
+    
     get_comment.short_description = 'Comment'
+
+
+    def get_status(self, obj):
+        if obj.status == 'pending':
+            return format_html('<span style="color: red;">{}</span>', obj.get_status_display())
+        else:
+            return format_html('<span style="color: green;">{}</span>', obj.get_status_display())
+
+    get_status.short_description = 'Status'
---
+++ src/reception/static/js/reception.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("Running")
+    setTimeout(function(){
+        window.location.reload();
+     }, 300000);
+})
