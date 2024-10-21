207b414161567d47363c7c4c11f64bebae578484
dev.afat <shaonmohammad13@gmail.com>
2024-10-21 13:54:45 +0600

    completed to added metadata in industry serve

--- src/website/admin/industries_we_serve.py
+++ src/website/admin/industries_we_serve.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
+import nested_admin
 
+from website.models import IndustryKeyword, IndustryMetadata
 from website.models_v2.industries_we_serve import ApplicationAreas, IndustryServe, ServeCategory
 
-class ApplicationAreasInline(admin.TabularInline):
+class ApplicationAreasInline(nested_admin.NestedStackedInline):
     model = ApplicationAreas
     extra = 0
 
+class IndustryKeywordInline(nested_admin.NestedStackedInline):
+    model = IndustryKeyword
+    extra = 1
+class IndustryMetadataInline(nested_admin.NestedStackedInline):
+    model = IndustryMetadata
+    extra = 1
+    inlines = [IndustryKeywordInline]
+
 @admin.register(ServeCategory)
-class ServeCategoryAdmin(admin.ModelAdmin):
+class ServeCategoryAdmin(nested_admin.NestedModelAdmin):
     list_display = ('title', 'slug',)
-    inlines = [ApplicationAreasInline]
+    inlines = [ApplicationAreasInline,IndustryMetadataInline]
     prepopulated_fields = {'slug': ('title',)}
 
 class ServeCategoryInline(admin.TabularInline):
