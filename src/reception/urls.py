6ebe1c5731cdc9045394d1fdaedcbe9f0665deea
dev.afat <shaonmohammad13@gmail.com>
2024-10-30 18:31:24 +0600

    completed pending count feature for CEO waiting list

--- src/reception/urls.py
+++ src/reception/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('form/<str:unique_url>/', views.visitor_form, name='visitor_form'),
     path('success/', views.success_page, name='success_page'),
     path('ajax/pending-reception-count/', views.pending_reception_count, name='pending_reception_count'),
+    path('ajax/pending-wating-count/',views.pending_waiting_count,name='pending_waiting_count'),
     path('ceo-appointment/',views.ceo_appointment,name='ceo_appoinment'),
     path('update-ceo-status',views.update_ceo_status,name='update_ceo_status')
 ]
