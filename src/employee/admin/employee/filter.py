796fcb4d55d13dae2e812c8000cedb95cd690fae
borhanst <mdborhan.st@gmail.com>
2024-12-23 13:26:05 +0600

    update filter

--- src/employee/admin/employee/filter.py
+++ src/employee/admin/employee/filter.py
@@ -3,24 +3,25 @@ from django.utils.translation import gettext_lazy as _
 
 from employee.models.employee_skill import EmployeeSkill
 
+
 class MonthFilter(SimpleListFilter):
-    title = _('month')
-    parameter_name = 'month'
+    title = _("month")
+    parameter_name = "month"
 
     def lookups(self, request, model_admin):
         return [
-            (1, _('January')),
-            (2, _('February')),
-            (3, _('March')),
-            (4, _('April')),
-            (5, _('May')),
-            (6, _('June')),
-            (7, _('July')),
-            (8, _('August')),
-            (9, _('September')),
-            (10, _('October')),
-            (11, _('November')),
-            (12, _('December')),
+            (1, _("January")),
+            (2, _("February")),
+            (3, _("March")),
+            (4, _("April")),
+            (5, _("May")),
+            (6, _("June")),
+            (7, _("July")),
+            (8, _("August")),
+            (9, _("September")),
+            (10, _("October")),
+            (11, _("November")),
+            (12, _("December")),
         ]
 
     def queryset(self, request, queryset):
@@ -30,13 +31,16 @@ class MonthFilter(SimpleListFilter):
 
 
 class TopSkillFilter(SimpleListFilter):
-    title = _('Skill')
-    parameter_name = 'employeeskill__id__exact'
+    title = _("Skill")
+    parameter_name = "employeeskill__id__exact"
 
     def lookups(self, request, model_admin):
-        skills = EmployeeSkill.objects.filter(percentage__gte=60)
-        return [(skill.id, skill.skill.title) for skill in skills]
-
+        skills = (
+            EmployeeSkill.objects.filter(percentage__gte=60)
+            .values("id", "skill__title")
+            .distinct()
+        )
+        return [(skill.get("id"), skill.get("skill__title")) for skill in skills]
 
     def queryset(self, request, queryset):
         if self.value():
