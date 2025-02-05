49b72b18850b42b6f3ed3253657d3b1dceb84543
arifrexa <arif.reza3126@gmail.com>
2025-02-05 11:51:58 +0600

    hour min in avg inside

--- src/employee/templatetags/custom_filters.py
+++ src/employee/templatetags/custom_filters.py
@@ -4,8 +4,22 @@ register = template.Library()
 
 @register.filter(name='get_item')
 def get_item(dictionary, datas):
-    total_avg_inside = 0
+    total_minutes = 0
 
     for date, data in datas.items():
-        total_avg_inside += int(data.get("inside_time_hour")) if data.get("inside_time_hour") else 0
-    return total_avg_inside
+        hours = int(data.get("inside_time_hour", 0))  # Get hours, default to 0
+        minutes = int(data.get("inside_time_minute", 0))  # Get minutes, default to 0
+
+        total_minutes += (hours * 60) + minutes  # Convert hours to minutes and sum up
+
+    total_hours = total_minutes // 60  # Get total hours
+    remaining_minutes = total_minutes % 60  # Get remaining minutes
+
+    return f"{total_hours}h : {remaining_minutes}m"
+# def get_item(dictionary, datas):
+#     total_avg_inside = 0
+#
+#     for date, data in datas.items():
+#         total_min = int(data.get("inside_time_minute")) if data.get("inside_time_minute") else 0
+#         total_avg_inside += int(data.get("inside_time_hour")) if data.get("inside_time_hour") else 0
+#     return total_avg_inside
