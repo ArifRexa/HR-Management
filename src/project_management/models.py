0a4b565e6e68f70e100f1ddabcc96eea250948ba
shaheen2013 <coredeveloper.2013@gmail.com>
2025-04-09 12:10:33 +0600

    add case study PDF field to project model with validation

--- src/project_management/models.py
+++ src/project_management/models.py
@@ -24,6 +24,7 @@ from config.model.AuthorMixin import AuthorMixin
 from employee.models import Employee
 from django.utils.html import format_html
 from django.utils.text import slugify
+from django.core.validators import FileExtensionValidator
 from icecream import ic
 
 # from employee.models import LeaveManagement
@@ -445,6 +446,19 @@ class Project(TimeStampMixin, AuthorMixin):
     )
     is_highlighted = models.BooleanField(verbose_name="Is Highlighted?", default=False)
     is_team = models.BooleanField(verbose_name="Is Team?", default=False)
+    case_study_pdf = models.FileField(
+        verbose_name="Case Study File (PDF)",
+        help_text="Only Upload PDF File",
+        upload_to="case_study_pdf", 
+        null=True, 
+        blank=True, 
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=["pdf"], 
+                message="Only PDF files are allowed. Please upload a valid PDF file."
+            )
+        ]
+    )
 
     # project_results = models.OneToOneField(
     #     ProjectResults,
