5befe839d1193d941b2f9785ebe14bd3d41e2f15
borhanst <mdborhan.st@gmail.com>
2025-08-13 19:53:34 +0600

    add pad in account journal

--- src/account/views.py
+++ src/account/views.py
@@ -2,7 +2,6 @@ import mimetypes
 from itertools import zip_longest
 from urllib.parse import urlparse
 
-import pandas as pd
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.db.models import (
@@ -17,27 +16,36 @@ from django.db.models import (
 from django.db.models.functions import ExtractYear, TruncDate
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.template.loader import get_template, render_to_string
+from django.template.loader import get_template
 from django.utils import timezone
 from django.views.decorators.http import require_http_methods
-from weasyprint import CSS, HTML, default_url_fetcher
+from weasyprint import HTML, default_url_fetcher
 
 from account.models import AccountJournal, Expense, Income, MonthlyJournal
+from config.utils.pdf import PDF
 
 
 def generate_voucher_pdf(voucher, voucher_type, template_name):
     expenses = (
-        voucher.expenses.values("expanse_group__account_code", "expanse_group__title")
+        voucher.expenses.values(
+            "expanse_group__account_code", "expanse_group__title"
+        )
         .annotate(expense_amount=Sum("amount"))
         .order_by("expanse_group__account_code")
-        .values("expanse_group__account_code", "expanse_group__title", "expense_amount")
+        .values(
+            "expanse_group__account_code",
+            "expanse_group__title",
+            "expense_amount",
+        )
         .exclude(add_to_balance_sheet=False)
     )
 
     # Generate voucher number
     id_list = []
     get_current_month_voucher = (
-        AccountJournal.objects.filter(type="daily").order_by("date").values_list("id")
+        AccountJournal.objects.filter(type="daily")
+        .order_by("date")
+        .values_list("id")
     )
 
     for d in get_current_month_voucher:
@@ -107,7 +115,8 @@ def account_journal(request, id):
     # data calculation
     expense_dates = (
         monthly_journal.expenses.filter(
-            date__year=monthly_journal.date.year, date__month=monthly_journal.date.month
+            date__year=monthly_journal.date.year,
+            date__month=monthly_journal.date.month,
         )
         .annotate(day=TruncDate("date", output_field=DateField()))
         .annotate(year=ExtractYear("date"))
@@ -149,7 +158,9 @@ def account_journal(request, id):
 
     # Create a response with the Excel file
     file_name = str(timezone.now())
-    response = HttpResponse(html_content, content_type="application/vnd.ms-excel")
+    response = HttpResponse(
+        html_content, content_type="application/vnd.ms-excel"
+    )
     response["Content-Disposition"] = (
         f"attachment; filename=account-journal-{file_name}.xls"
     )
@@ -177,10 +188,12 @@ def costs_by_expense_group(request, id):
             vds_rate=F("expanse_group__vds_rate"),
             tds_rate=F("expanse_group__tds_rate"),
             vds_amount=ExpressionWrapper(
-                (F("vds_rate") * F("expense_amount") / 100), output_field=DecimalField()
+                (F("vds_rate") * F("expense_amount") / 100),
+                output_field=DecimalField(),
             ),
             tds_amount=ExpressionWrapper(
-                (F("tds_rate") * F("expense_amount") / 100), output_field=DecimalField()
+                (F("tds_rate") * F("expense_amount") / 100),
+                output_field=DecimalField(),
             ),
         )
         .order_by("expanse_group__account_code")
@@ -202,7 +215,9 @@ def costs_by_expense_group(request, id):
 
     # Create a response with the Excel file
     file_name = str(timezone.now())
-    response = HttpResponse(html_content, content_type="application/vnd.ms-excel")
+    response = HttpResponse(
+        html_content, content_type="application/vnd.ms-excel"
+    )
     response["Content-Disposition"] = (
         f"attachment; filename=account-journal-{file_name}.xls"
     )
@@ -271,11 +286,32 @@ def balance_sheet(request, id):
 
     # Create a response with the Excel file
     file_name = str(timezone.now())
-    response = HttpResponse(html_content, content_type="application/vnd.ms-excel")
+    response = HttpResponse(
+        html_content, content_type="application/vnd.ms-excel"
+    )
     response["Content-Disposition"] = (
         f"attachment; filename=income-statement -{file_name}.xls"
     )
     return response
+
+
+from django.templatetags.static import static
+
+
+def custom_fetcher(url, *args, **kwargs):
+    parsed = urlparse(url)
+    if parsed.path.startswith(settings.STATIC_URL):
+        path = parsed.path.replace(
+            settings.STATIC_URL, settings.STATIC_ROOT + "/", 1
+        )
+        mime, enc = mimetypes.guess_type(path)
+        return {
+            "file_obj": open(path, "rb"),
+            "mime_type": mime,
+            "encoding": enc,
+            "filename": path.split("/")[-1],
+        }
+    return default_url_fetcher(url, *args, **kwargs)
 
 
 @require_http_methods(["GET"])
@@ -286,43 +322,107 @@ def monthly_expense_statement(request, id, *args, **kwargs):
     field_mapping = {"date": "created_at", "amount": "amount"}
     expense = (
         Expense.objects.filter(
-            date__month=monthly_journal.date.month, date__year=monthly_journal.date.year
+            date__month=monthly_journal.date.month,
+            date__year=monthly_journal.date.year,
         )
         .order_by(field_mapping.get(order_by))
-        .values("date", "expanse_group__title", "expense_category__title", "amount")
+        .values(
+            "date", "expanse_group__title", "expense_category__title", "amount"
+        )
     )
-    df = pd.DataFrame(list(expense))
-    df.rename(
-        columns={
-            "date": "Date",
-            "expanse_group__title": "Expense Group",
-            "expense_category__title": "Expense Category",
-            "amount": "Amount",
-        },
-        inplace=True,
-    )
-    df.loc["Total"] = df.sum(numeric_only=True, axis=0)
-    df = df.fillna("")
-    df.at["Total", "Expense Category"] = "Total"
-    template = get_template("pdf/monthly_expense.html")
-    context = {
-        "table": df.to_html(index=False),
+    # logo_url = request.build_absolute_uri(static('myapp/logo.png'))
+    watermark = request.build_absolute_uri(static("letter_head_new.jpeg"))
+    pdf = PDF()
+
+    pdf.context = {
+        "expenses": expense,
+        "total_amount": expense.aggregate(total=Sum("amount"))["total"],
         "month": monthly_journal.date.strftime("%B"),
         "year": monthly_journal.date.strftime("%Y"),
     }
-    html_content = template.render(context)
+    pdf.template_path = "pdf/monthly_expense.html"
+    # html_content  = render_to_string("pdf/monthly_expense.html", context=context)
     month = monthly_journal.date.strftime("%B")
     year = monthly_journal.date.strftime("%Y")
     file_name_date = f"{month}/{year}"
-    file_name = f"{file_name_date}_ME_order_by_{order_by}.pdf"
+    pdf.file_name = f"{file_name_date}_ME_order_by_{order_by}.pdf"
 
     # Generate PDF
-    html = HTML(string=html_content)
-    pdf_file = html.write_pdf()
-    response = HttpResponse(pdf_file, content_type="application/pdf")
-    response["Content-Disposition"] = f'attachment; filename="{file_name}"'
+    # html = HTML(string=html_content, base_url=request.build_absolute_uri("/"), url_fetcher=custom_fetcher)
+    # pdf_file = html.write_pdf()
+    # response = HttpResponse(pdf_file, content_type="application/pdf")
+    # response["Content-Disposition"] = f'attachment; filename="{file_name}"'
 
-    return response
+    return pdf.render_to_pdf(download=True)
+
+
+# @require_http_methods(["GET"])
+# @login_required(login_url="/admin/login/")
+# def monthly_expense_attachment(request, id, *args, **kwargs):
+#     monthly_journal = MonthlyJournal.objects.get(id=id)
+#     order_by = request.GET.get("order_by", "date")
+#     field_mapping = {"date": "created_at", "amount": "amount"}
+
+#     expense_paths = (
+#         Expense.objects.filter(
+#             date__month=monthly_journal.date.month,
+#             date__year=monthly_journal.date.year,
+#         )
+#         .order_by(field_mapping.get(order_by))
+#         .annotate(file=F("expanseattachment__attachment"))
+#         .values_list("file", flat=True)
+#     )
+
+#     absolute_urls = []
+#     for relpath in expense_paths:
+#         if relpath:
+#             rel = (
+#                 relpath
+#                 if relpath.startswith("/")
+#                 else settings.MEDIA_URL + relpath
+#             )
+#             absolute_urls.append(request.build_absolute_uri(rel))
+
+#     pdf = PDF()
+#     pdf.context = {
+#         "expenses": absolute_urls,
+#         "month": monthly_journal.date.strftime("%B"),
+#         "year": monthly_journal.date.strftime("%Y"),
+#     }
+#     pdf.template_path = "pdf/monthly_expense_attachment.html"
+#     # html_str = render_to_string("pdf/monthly_expense_attachment.html", context)
+
+#     def custom_fetcher(url, *args, **kwargs):
+#         parsed = urlparse(url)
+#         if parsed.path.startswith(settings.MEDIA_URL):
+#             path = parsed.path.replace(
+#                 settings.MEDIA_URL, settings.MEDIA_ROOT + "/", 1
+#             )
+#             mime, enc = mimetypes.guess_type(path)
+#             return {
+#                 "file_obj": open(path, "rb"),
+#                 "mime_type": mime,
+#                 "encoding": enc,
+#                 "filename": path.split("/")[-1],
+#             }
+#         return default_url_fetcher(url, *args, **kwargs)
+
+#     # html = HTML(
+#     #     string=html_str,
+#     #     base_url=request.build_absolute_uri("/"),
+#     #     url_fetcher=custom_fetcher,
+#     # )
+#     # css = CSS(string="@page { size: A4; margin: 1cm }")
+#     # pdf_bytes = html.write_pdf(stylesheets=[css])
+
+#     # response = HttpResponse(pdf_bytes, content_type="application/pdf")
+#     month = monthly_journal.date.strftime("%B")
+#     year = monthly_journal.date.strftime("%Y")
+#     pdf.file_name = f"{month}/{year}"
+#     # response["Content-Disposition"] = (
+#     #     f'attachment; filename="{file_name}_MA_order_by_{order_by}.pdf"'
+#     # )
+#     return pdf.render_to_pdf(download=True)
 
 
 @require_http_methods(["GET"])
@@ -334,7 +434,8 @@ def monthly_expense_attachment(request, id, *args, **kwargs):
 
     expense_paths = (
         Expense.objects.filter(
-            date__month=monthly_journal.date.month, date__year=monthly_journal.date.year
+            date__month=monthly_journal.date.month,
+            date__year=monthly_journal.date.year,
         )
         .order_by(field_mapping.get(order_by))
         .annotate(file=F("expanseattachment__attachment"))
@@ -344,21 +445,27 @@ def monthly_expense_attachment(request, id, *args, **kwargs):
     absolute_urls = []
     for relpath in expense_paths:
         if relpath:
-            rel = relpath if relpath.startswith("/") else settings.MEDIA_URL + relpath
+            rel = (
+                relpath
+                if relpath.startswith("/")
+                else settings.MEDIA_URL + relpath
+            )
             absolute_urls.append(request.build_absolute_uri(rel))
 
-    context = {
+    pdf = PDF()
+    pdf.context = {
         "expenses": absolute_urls,
         "month": monthly_journal.date.strftime("%B"),
         "year": monthly_journal.date.strftime("%Y"),
     }
-
-    html_str = render_to_string("pdf/monthly_expense_attachment.html", context)
+    pdf.template_path = "pdf/monthly_expense_attachment.html"
 
     def custom_fetcher(url, *args, **kwargs):
         parsed = urlparse(url)
         if parsed.path.startswith(settings.MEDIA_URL):
-            path = parsed.path.replace(settings.MEDIA_URL, settings.MEDIA_ROOT + "/", 1)
+            path = parsed.path.replace(
+                settings.MEDIA_URL, settings.MEDIA_ROOT + "/", 1
+            )
             mime, enc = mimetypes.guess_type(path)
             return {
                 "file_obj": open(path, "rb"),
@@ -368,19 +475,8 @@ def monthly_expense_attachment(request, id, *args, **kwargs):
             }
         return default_url_fetcher(url, *args, **kwargs)
 
-    html = HTML(
-        string=html_str,
-        base_url=request.build_absolute_uri("/"),
-        url_fetcher=custom_fetcher,
-    )
-    css = CSS(string="@page { size: A4; margin: 1cm }")
-    pdf_bytes = html.write_pdf(stylesheets=[css])
-
-    response = HttpResponse(pdf_bytes, content_type="application/pdf")
     month = monthly_journal.date.strftime("%B")
     year = monthly_journal.date.strftime("%Y")
-    file_name = f"{month}/{year}"
-    response["Content-Disposition"] = (
-        f'attachment; filename="{file_name}_MA_order_by_{order_by}.pdf"'
-    )
-    return response
+    pdf.file_name = f"{month}/{year}"
+
+    return pdf.render_to_pdf(download=True)
