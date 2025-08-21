e2183dff154ad019536f054f64f008c0ea8a896d
arifrexa <arif.reza3126@gmail.com>
2025-08-21 15:56:37 +0600

    industry hero section secondary title added

--- src/website/models_v2/industries_we_serve.py
+++ src/website/models_v2/industries_we_serve.py
@@ -41,6 +41,7 @@ class IndustryDetailsHeroSection(models.Model):
     )
     seo_title = models.CharField(max_length=200, blank=True, null=True)
     section_title = models.CharField(max_length=200, blank=True, null=True)
+    secondary_title = models.CharField(max_length=200, blank=True, null=True)
     section_description = HTMLField(blank=True, null=True)
     image = models.ImageField(upload_to='industry_details_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
