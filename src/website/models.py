fd84c16ab4e12afb587ac22ee0addf6127bf8766
arifrexa <arif.reza3126@gmail.com>
2025-09-11 13:53:28 +0600

    additional pages fields modified

--- src/website/models.py
+++ src/website/models.py
@@ -1358,7 +1358,8 @@ class Subscription(TimeStampMixin):
 class AdditionalPages(TimeStampMixin):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    description = models.TextField(null=True, blank=True)
+    meta_title = models.CharField(max_length=255, verbose_name="Meta Title", null=True, blank=True)
+    description = models.TextField(null=True, blank=True, verbose_name='Meta Description')
 
     def __str__(self):
         return self.title
@@ -1375,7 +1376,7 @@ class AdditionalPageHeroSection(TimeStampMixin):
     section_title = models.CharField(max_length=255, null=True, blank=True)
     secodary_title = models.CharField(max_length=255, null=True, blank=True)
     # slug = models.SlugField(unique=True)
-    section_description = models.TextField(null=True, blank=True)
+    section_description = HTMLField(null=True, blank=True)
 
     def __str__(self):
         return self.section_title or "Hero Section"
@@ -1388,9 +1389,9 @@ class WhatIs(TimeStampMixin):
         null=True,
         blank=True
     )
-    seo_title = models.CharField(max_length=255, verbose_name="SEO Title")
-    section_title = models.CharField(max_length=255)
-    section_description = models.TextField(null=True, blank=True)
+    seo_title = models.CharField(max_length=255, verbose_name="SEO Title", blank=True, null=True)
+    section_title = models.CharField(max_length=255, blank=True, null=True)
+    section_description = HTMLField(null=True, blank=True)
 
     def __str__(self):
         return self.section_title
@@ -1405,7 +1406,7 @@ class AdditionalPageKeyThings(TimeStampMixin):
     )
     seo_title = models.CharField(max_length=200, blank=True, null=True, verbose_name="SEO Title")
     section_title = models.CharField(max_length=200, blank=True, null=True)
-    section_description = models.TextField(blank=True, null=True)
+    section_description = HTMLField(blank=True, null=True)
 
     def __str__(self):
         return self.section_title
@@ -1419,7 +1420,7 @@ class AdditionalPageKeyThingsCards(TimeStampMixin):
         blank=True
     )
     card_title = models.CharField(max_length=200, blank=True, null=True)
-    card_description = models.TextField(blank=True, null=True)
+    card_description = HTMLField(blank=True, null=True)
     def __str__(self):
         return self.card_title
     
@@ -1460,7 +1461,7 @@ class AdditionalPageOurProcess(models.Model):
         null=True,
         blank=True
     )
-    section_title = models.CharField(max_length=200)
+    section_title = models.CharField(max_length=200, blank=True, null=True)
     section_description = HTMLField(blank=True, null=True)
     order = models.PositiveIntegerField(default=0, help_text="Order of display for process steps")
 
