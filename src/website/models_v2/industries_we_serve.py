ec0ed2e43256fd74bcac0806fb61f38bd0ae401d
arifrexa <arif.reza3126@gmail.com>
2025-09-19 18:10:42 +0600

    logo added to projects and icon field modified of industry details

--- src/website/models_v2/industries_we_serve.py
+++ src/website/models_v2/industries_we_serve.py
@@ -55,7 +55,7 @@ class IndustryDetailsHeroSection(models.Model):
     section_title = models.CharField(max_length=200, blank=True, null=True)
     secondary_title = models.CharField(max_length=200, blank=True, null=True)
     section_description = HTMLField(blank=True, null=True)
-    image = models.ImageField(upload_to='industry_details_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='industry_details_images/', blank=True, null=True, verbose_name="Icon")
     featured_image = models.ImageField(upload_to="technology_featured_images/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
