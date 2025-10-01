7634bb3459fb13ba90fa5b8cbfab03c78cc7fb78
arifrexa <arif.reza3126@gmail.com>
2025-10-01 18:37:30 +0600

    contact form filters added for action and form type

--- src/website/admin/admin.py
+++ src/website/admin/admin.py
@@ -2352,8 +2352,11 @@ class ContactFormAdmin(admin.ModelAdmin):
         # "client_query_truncated",
         # "project_details_truncated",
         "short_brief_truncated",
+        "action_by",
         "created_at"
     )
+    
+    list_filter = ("form_type", "action_by")
 
     def short_brief_truncated(self, obj):
         if not obj.short_brief:
