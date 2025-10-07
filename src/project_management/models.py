5527211dd8bc6e3ce677cffe5d1908d6f6844ede
borhanst <mdborhan.st@gmail.com>
2025-10-07 16:11:47 +0600

    update project estimation total used hour daily update to weekly update

--- src/project_management/models.py
+++ src/project_management/models.py
@@ -1712,8 +1712,8 @@ class ProjectEstimation(TimeStampMixin, AuthorMixin):
 
     @property
     def total_hours_used(self):
-        total = DailyProjectUpdate.objects.filter(
-            project=self.project, created_at__date__gte=self.date, status="approved"
+        total = ProjectHour.objects.filter(
+            project=self.project, date__gte=self.date, status="approved", hour_type="project"
         ).aggregate(total_hours=Sum("hours"))
         return total["total_hours"] if total["total_hours"] else 0
 
