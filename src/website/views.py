832278b7a3819fb1581cd4027529164d3238f2d3
arifrexa <arif.reza3126@gmail.com>
2025-11-28 15:12:34 +0600

    blog list max page size removed

--- src/website/views.py
+++ src/website/views.py
@@ -1336,7 +1336,7 @@ class TechnologyNavigationView(APIView):
 class BlogPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    # max_page_size = 100
 
 
 
