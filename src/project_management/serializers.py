673797dadd0a35f1ac59d4e81d001fc15de0e528
arifrexa <arif.reza3126@gmail.com>
2026-01-01 19:00:27 +0600

    revert the case study to before

--- src/project_management/serializers.py
+++ src/project_management/serializers.py
@@ -107,8 +107,8 @@ class TechnologySerializer(serializers.ModelSerializer):
 
 class ProjectListSerializer(serializers.ModelSerializer):
     """Lightweight serializer for project list"""
-    # client_name = serializers.CharField(source='client.name', read_only=True)
-    # client_image = serializers.ImageField(source='client.image', read_only=True)
+    client_name = serializers.CharField(source='client.name', read_only=True)
+    client_image = serializers.ImageField(source='client.image', read_only=True)
     industries = IndustrySerializer(many=True, read_only=True)
     services = ServiceSerializer(many=True, read_only=True)
     technology = TechnologySerializer(many=True, read_only=True)
@@ -116,16 +116,15 @@ class ProjectListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         # fields = [
-        #     'id', 'title', 'slug', 'description', 'client_name', 
-        #     'client_image', 'client_review', 'client_designation', 'featured_image', 'short_video', 'thumbnail', 'live_link', 'project_logo',
-        #     'industries', 'services', 'technology',
-        #     'active', 'show_in_website', 'is_special', 'created_at', 'updated_at'
+        #     'id', 'title', 'slug', 'short_video', 'thumbnail', 'live_link', 
+        #     'project_logo', 'industries', 'services', 'technology'
         # ]
         fields = [
-            'id', 'title', 'slug', 'short_video', 'thumbnail', 'live_link', 
-            'project_logo', 'industries', 'services', 'technology'
+            'id', 'title', 'slug', 'description', 'client_name', 
+            'client_image', 'client_review', 'client_designation', 'featured_image', 'short_video', 'thumbnail', 'live_link', 'project_logo',
+            'industries', 'services', 'technology',
+            'active', 'show_in_website', 'is_special', 'created_at', 'updated_at'
         ]
-        
         ref_name = 'ProjectListReadOnly'
 
 
